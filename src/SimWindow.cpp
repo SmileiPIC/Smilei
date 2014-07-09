e6ae5efee2254b748abeef34803ee8753da5c095
Arnaud Beck <beck@llr.in2p3.fr>
2014-07-09 15:51:08 +0200

    Moving window with clrw > 1 should work now.
    A full cluster width is exchanged each time the window
    moves.

--- src/SimWindow.cpp
+++ src/SimWindow.cpp
@@ -24,19 +24,21 @@ SimWindow::~SimWindow()
 void SimWindow::operate(vector<Species*> vecSpecies, ElectroMagn* EMfields, Interpolator* Interp, Projector* Proj, SmileiMPI* smpi)
 {
 
-    smpi->getCellStartingGlobalIndex(0)+= 1;
-    smpi->getDomainLocalMin(0)+= cell_length_x_;
-    smpi->getDomainLocalMax(0)+= cell_length_x_;
+    unsigned int clrw = vecSpecies[0]->clrw; //clrw must be the same for all species
+
+    smpi->getCellStartingGlobalIndex(0)+= clrw;
+    smpi->getDomainLocalMin(0)+= cell_length_x_*clrw;
+    smpi->getDomainLocalMax(0)+= cell_length_x_*clrw;
 
     
     for (unsigned int ispec=0 ; ispec<vecSpecies.size(); ispec++) {
-	vecSpecies[ispec]->movingWindow_x(1, smpi);
+	vecSpecies[ispec]->movingWindow_x(clrw,smpi);
     }
 
-    Interp->mv_win(1);
-    Proj->mv_win(1);
-    EMfields->movingWindow_x(1, smpi);
-    x_moved += cell_length_x_;
+    Interp->mv_win(clrw);
+    Proj->mv_win(clrw);
+    EMfields->movingWindow_x(clrw, smpi);
+    x_moved += cell_length_x_*clrw;
 
 
 }
