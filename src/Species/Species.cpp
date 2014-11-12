4698fd638d9a95020c813478cf2874a8f3acb7c3
Arnaud Beck <beck@llr.in2p3.fr>
2014-11-12 10:43:18 +0100

    res_window -> nspace_window

--- src/Species/Species.cpp
+++ src/Species/Species.cpp
@@ -810,7 +810,7 @@ void Species::updateMvWinLimits(double x_moved) {
                             //Before end of density ramp up.
                             (simWindow->getXmoved() < species_param.vacuum_length[0] + species_param.dens_length_x[1] + clrw*cell_length[0] || 
                             //After begining of density ramp down. 
-                            simWindow->getXmoved() +  simWindow->getRes_space_win_x()*cell_length[0] > species_param.vacuum_length[0] + species_param.dens_length_x[1]+ species_param.dens_length_x[0]
+                            simWindow->getXmoved() +  simWindow->getNspace_win_x()*cell_length[0] > species_param.vacuum_length[0] + species_param.dens_length_x[1]+ species_param.dens_length_x[0]
                             )
                         )
                     ) 
