3e1c9fe4c42afcc6c128f7f96e453f134fadba36
Julien <julien.derouillat@cea.fr>
2014-11-14 15:58:42 +0100

    SpeciesFactory output global number of particles

--- src/Species/SpeciesFactory.h
+++ src/Species/SpeciesFactory.h
@@ -40,7 +40,7 @@ public:
             }
             nPart = vecSpecies[ispec]->getNbrOfParticles();
             MPI_Reduce(smpi->isMaster()?MPI_IN_PLACE:&nPart, &nPart, 1, MPI_UNSIGNED, MPI_SUM, 0, MPI_COMM_WORLD);
-            MESSAGE(1,"Species " << ispec << " (" << params.species_param[ispec].species_type << ") created with " << vecSpecies[ispec]->getNbrOfParticles() << " particles" );
+            MESSAGE(1,"Species " << ispec << " (" << params.species_param[ispec].species_type << ") created with " << nPart << " particles" );
         } // END for ispec
 
         // add the found electron species to the ionizable species
