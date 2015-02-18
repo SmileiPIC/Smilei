936a39054d1ad10cd6c813db1f7f877814e07f42
Julien <julien.derouillat@cea.fr>
2015-02-18 11:29:20 +0100

    Still optimizing (suppress J*_p, validated on2d_3_plasma_collision

--- src/Projector/Projector2D2Order.cpp
+++ src/Projector/Projector2D2Order.cpp
@@ -418,7 +418,7 @@ void Projector2D2Order::operator() (double* Jx, double* Jy, double* Jz, double* 
     double delta, delta2;
     double Sx0[5]={}, Sx1[5]={}, Sy0[5]={}, Sy1[5]={}, DSx[5]={}, DSy[5]={}; // arrays used for the Esirkepov projection method
     double Wx[4][5]={}, Wy[5][4]={}, Wz[5][5]={};                   // idem
-    double Jx_p[5][5]={};//, Jy_p[5][5]={};                         // idem
+    //double Jx_p[5][5]={};//, Jy_p[5][5]={};                         // idem
 
     // --------------------------------------------------------
     // Locate particles & Calculate Esirkepov coef. S, DS and W
@@ -491,11 +491,11 @@ void Projector2D2Order::operator() (double* Jx, double* Jy, double* Jz, double* 
     // Local current created by the particle
     // calculate using the charge conservation equation
     // ------------------------------------------------
-    for (unsigned int i=1 ; i<5 ; i++) {
+    /*for (unsigned int i=1 ; i<5 ; i++) {
         for (unsigned int j=0 ; j<5 ; j++) {
             Jx_p[i][j] = Jx_p[i-1][j] - crx_p * Wx[i-1][j];
         }
-    }
+    }*/
     /*for (unsigned int i=0 ; i<5 ; i++) {
         for (unsigned int j=1 ; j<5 ; j++) {
             Jy_p[i][j] = Jy_p[i][j-1] - cry_p * Wy[i][j-1];
@@ -529,16 +529,21 @@ void Projector2D2Order::operator() (double* Jx, double* Jy, double* Jz, double* 
     }//i
 
 
+    for (unsigned int i=1 ; i<5 ; i++) Sx0[i] = 0.;
     for (unsigned int i=1 ; i<5 ; i++) {
         iloc = (i+ipo-2)*b_dim1;
 	jloc = iloc+jpo-2; 
-	Jx[jloc]  += Jx_p[i][0];
+	Sx0[0] -= crx_p * Wx[i-1][0];
+	//Jx[jloc]  += Jx_p[i][0];
+	Jx[jloc]  += Sx0[0];
 	Jz[jloc]  += crz_p * Wz[i][0];
 	rho[jloc] += charge_weight * Sx1[i]*Sy1[0];	
 	tmp = 0;
         for (unsigned int j=1 ; j<5 ; j++) {
             jloc = iloc+j+jpo-2; 
-            Jx[jloc]  += Jx_p[i][j];
+            Sx0[j] -= crx_p * Wx[i-1][j];
+            //Jx[jloc]  += Jx_p[i][j];
+	    Jx[jloc]  += Sx0[j];
             //Jy[jloc]  += Jy_p[i][j];
 	    tmp -= cry_p * Wy[i][j-1];
             Jy[jloc]  += tmp;
