ee52cddd3052283dc08b87e8503e5e6e9e50da76
Julien <julien.derouillat@cea.fr>
2015-04-24 17:38:22 +0200

    readd SmileiMPI

---
+++ src/SmileiMPI/SpeciesMPI.cpp
@@ -0,0 +1,38 @@
+
+#include <SpeciesMPI.h>
+
+SpeciesMPI::SpeciesMPI()
+{
+}
+
+void SpeciesMPI::init()
+{
+    int ndims_ = 2;
+    int nbNeighbors_ = 2;
+
+    for (int iDir=0 ; iDir<ndims_ ; iDir++) {
+	for (int i=0 ; i<nbNeighbors_ ; i++) {
+	    patch_buff_index_send[iDir][i].resize(0);
+	    patch_buff_index_recv_sz[iDir][i] = 0;
+	    patch_buff_index_send_sz[iDir][i] = 0;
+	    corner_buff_index_send[iDir][i].resize(0);
+	    corner_buff_index_recv_sz[iDir][i] = 0;
+	    corner_buff_index_send_sz[iDir][i] = 0;
+	}
+    }
+
+    for (int iDim=0 ; iDim<2 ; iDim++) {
+	for (int iNeighbor=0 ; iNeighbor<nbNeighbors_ ; iNeighbor++) {
+	    patchVectorRecv[iDim][iNeighbor].initialize(0,2);
+	    patchVectorSend[iDim][iNeighbor].initialize(0,2);
+	    // Globalize IP (store in SmileiMPI_Cart2D, OK while init+init+finalize / dir)
+	    cornerVectorRecv[iDim][iNeighbor].initialize(0,2);
+	    cornerVectorSend[iDim][iNeighbor].initialize(0,2);
+	}
+    }
+}
+
+SpeciesMPI::~SpeciesMPI()
+{
+}
+
---
+++ src/SmileiMPI/SpeciesMPI.h
@@ -0,0 +1,37 @@
+#ifndef SPECIESMPI_H
+#define SPECIESMPI_H
+
+#include <mpi.h>
+
+#include "Particles.h"
+
+class SpeciesMPI {
+ public:
+    SpeciesMPI();
+    ~SpeciesMPI();
+    void init();
+
+    friend class SmileiMPI_Cart2D;
+
+
+    Particles patchVectorRecv[2][2];
+    Particles patchVectorSend[2][2];
+    Particles cornerVectorRecv[2][2];
+    Particles cornerVectorSend[2][2];
+
+    std::vector<int> patch_buff_index_send[2][2];
+    std::vector<int> corner_buff_index_send[2][2];
+    int patch_buff_index_send_sz[2][2];
+    int corner_buff_index_send_sz[2][2];
+    int patch_buff_index_recv_sz[2][2];
+    int corner_buff_index_recv_sz[2][2];
+
+    MPI_Request patch_srequest[2][2];
+    MPI_Request patch_rrequest[2][2];
+    MPI_Request corner_srequest[2][2];
+    MPI_Request corner_rrequest[2][2];
+
+};
+
+#endif
+
