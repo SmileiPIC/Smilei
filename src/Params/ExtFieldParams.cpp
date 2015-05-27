edab7395f29ab557b766609bc46fe0ff9bffebcd
fffred <master0@hotmail.com>
2015-05-27 16:24:06 +0200

    Forgotten changes for external fields

--- src/Params/ExtFieldParams.cpp
+++ src/Params/ExtFieldParams.cpp
@@ -7,29 +7,29 @@ using namespace std;
 ExtFieldParams::ExtFieldParams(PicParams& params, InputData &ifile) :
 geometry(params.geometry)
 {
-	
+
     // -----------------
     // ExtFields properties
     // -----------------
     int n_extfield=0;
-    while (ifile.existComponent("ext_field",n_extfield)) {
+    while (ifile.existComponent("ExtField",n_extfield)) {
         
         ExtFieldStructure tmpExtField;
-        ifile.extract("field",tmpExtField.fields,"ext_field",n_extfield);
-        ifile.extract("profile",tmpExtField.profile,"ext_field",n_extfield);
+        ifile.extract("field",tmpExtField.fields,"ExtField",n_extfield);
+        ifile.extract("profile",tmpExtField.profile,"ExtField",n_extfield);
         
         if (tmpExtField.profile.empty()) {
-            PyObject *mypy = ifile.extract_py("profile","ext_field",n_extfield);
+            PyObject *mypy = ifile.extract_py("profile","ExtField",n_extfield);
             if (mypy && PyCallable_Check(mypy)) {
                 tmpExtField.py_profile=mypy;
                 tmpExtField.profile="python";
             }
         } else {
-            ifile.extract("int_params",tmpExtField.int_params,"ext_field",n_extfield);
-            ifile.extract("double_params",tmpExtField.double_params,"ext_field",n_extfield);
-            ifile.extract("length_params_x",tmpExtField.length_params_x,"ext_field",n_extfield);
-            ifile.extract("length_params_y",tmpExtField.length_params_y,"ext_field",n_extfield);
-            ifile.extract("length_params_z",tmpExtField.length_params_z,"ext_field",n_extfield);
+            ifile.extract("int_params",tmpExtField.int_params,"ExtField",n_extfield);
+            ifile.extract("double_params",tmpExtField.double_params,"ExtField",n_extfield);
+            ifile.extract("length_params_x",tmpExtField.length_params_x,"ExtField",n_extfield);
+            ifile.extract("length_params_y",tmpExtField.length_params_y,"ExtField",n_extfield);
+            ifile.extract("length_params_z",tmpExtField.length_params_z,"ExtField",n_extfield);
             
             transform(tmpExtField.length_params_x.begin(), tmpExtField.length_params_x.end(), tmpExtField.length_params_x.begin(),
                       bind1st(multiplies<double>(),params.conv_fac));
@@ -37,9 +37,8 @@ geometry(params.geometry)
                       bind1st(multiplies<double>(),params.conv_fac));
             transform(tmpExtField.length_params_z.begin(), tmpExtField.length_params_z.end(), tmpExtField.length_params_z.begin(),
                       bind1st(multiplies<double>(),params.conv_fac));
-        }   
+        }
         structs.push_back(tmpExtField);
-
         n_extfield++;
     }
     
