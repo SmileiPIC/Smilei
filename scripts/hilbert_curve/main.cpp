f7966a40906828756ffb787c634e9b41532b4a46
Arnaud Beck <beck@llr.in2p3.fr>
2015-09-09 15:58:36 +0200

    Improved hilbert visu

--- scripts/hilbert_curve/main.cpp
+++ scripts/hilbert_curve/main.cpp
@@ -1,3 +1,5 @@
+/**This scripts generates hindex and coordinates of patches just like in Smilei**/
+
 #include <cstdlib>
 #include <unistd.h>
 
@@ -17,40 +19,25 @@ int main (int argc, char* argv[])
 {
     std::cout.setf( std::ios::fixed, std:: ios::floatfield ); // floatfield set to fixed
 
+/******* USER DEFINED PARAMETERS*****/
    unsigned int m0 = 5;
    unsigned int m1 = 3;
+/************************************/
+
+
    unsigned int X,Y;
-   int nmpi = 15;
    ofstream myfile;
    myfile.open ("data.txt");
 
-   
-
-
    cout << (1<<m0) << "patches selon x, " << (1<<m1) << " patches selon y." << endl;
 
-   int step, total_patch;
+   int total_patch;
    total_patch = (1<<m0)*(1<<m1);
-   //step = total_patch/nmpi + 1;
-   step = total_patch/nmpi ;
    for (unsigned int ipatch = 0; ipatch < total_patch; ipatch++){
        generalhilbertindexinv(m0, m1, &X, &Y, ipatch);
        myfile << ipatch << " " << X << " " << Y << endl;
 
    }
-   int first_patch = 0;
-   int last_patch=-1;
-
-
-   for (unsigned int impi=0; impi < nmpi-1; impi++){
-       first_patch = last_patch+1;
-       last_patch = first_patch-1+step;
-       cout << "rank "<<impi<<" first "<<first_patch<<" last "<<last_patch<< " npatches= " << last_patch+1-first_patch << endl;
-   } 
-       unsigned int impi = nmpi-1;
-       first_patch = last_patch+1;
-       last_patch = total_patch-1;
-       cout << "rank "<<impi<<" first "<<first_patch<<" last "<<last_patch<< " npatches= " << last_patch+1-first_patch << endl;
     
    myfile.close();
    return 0;
