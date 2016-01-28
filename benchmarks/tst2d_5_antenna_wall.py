e3e341b769e683c5b9538bece664f73a7256c8f6
Julien <julien.derouillat@cea.fr>
2016-01-28 15:53:36 +0100

    Added a patch decomposition in antenna/wall test

--- benchmarks/tst2d_5_antenna_wall.py
+++ benchmarks/tst2d_5_antenna_wall.py
@@ -34,6 +34,8 @@ interpolation_order = 2
 cell_length = [l0/resx,l0/resx]
 sim_length  = Lsim
 
+number_of_patches = [ 4, 4  ]
+
 # SIMULATION TIME
 # timestep: duration of the timestep
 # sim_time: duration of the simulation in units of the normalization period 
