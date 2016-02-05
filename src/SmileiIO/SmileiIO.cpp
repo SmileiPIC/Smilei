e10751c0ed87f7ec30f8b2809de5916aa337db6c
Julien <julien.derouillat@cea.fr>
2016-02-05 16:43:53 +0100

    Fixed a bug in a 1 patch per process configuration

--- src/SmileiIO/SmileiIO.cpp
+++ src/SmileiIO/SmileiIO.cpp
@@ -32,7 +32,7 @@ global_file_id_avg(0)
     // Create property list for collective dataset write.
     //
     write_plist = H5Pcreate(H5P_DATASET_XFER);
-    if (params.simu_is_cartesian)
+    if (!params.simu_is_cartesian)
 	H5Pset_dxpl_mpio(write_plist, H5FD_MPIO_INDEPENDENT);
     else
 	H5Pset_dxpl_mpio(write_plist, H5FD_MPIO_COLLECTIVE);
