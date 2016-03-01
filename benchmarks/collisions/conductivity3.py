d6600051a4e0ecd553c55ed7ab2fcce71171c9db
Fred Perez <frederic.perez@polytechnique.edu>
2016-03-01 14:12:03 +0100

    Put all the collision tests in benchmarks (they were brutally removed)

---
+++ benchmarks/collisions/conductivity3.py
@@ -0,0 +1,224 @@
+# ---------------------------------------------
+# SIMULATION PARAMETERS FOR THE PIC-CODE SMILEI
+# ---------------------------------------------
+# Remember: never override the following names:
+#           SmileiComponent, Species, Laser, Collisions, DiagProbe, DiagParticles,
+#           DiagScalar, DiagPhase or ExtField
+
+import math
+L0 = 2.*math.pi # conversion from normalization length to wavelength
+
+wavelength_SI = 1.e-6
+
+# dim: Geometry of the simulation
+#      1d3v = cartesian grid with 1d in space + 3d in velocity
+#      2d3v = cartesian grid with 2d in space + 3d in velocity
+#      3d3v = cartesian grid with 3d in space + 3d in velocity
+#      2drz = cylindrical (r,z) grid with 3d3v particles
+dim = "1d3v"
+
+# number_of_patches: list of the number of patches in each dimension
+number_of_patches = [ 4 ]
+
+# order of interpolation
+interpolation_order = 2
+
+# SIMULATION TIME 
+# timestep = float, time steps
+# sim_time = float, duration of the simulation
+timestep = 0.002 * L0
+sim_time  = 0.8 * L0
+
+
+#  optional parameter time_fields_frozen, during which fields are not updated
+time_fields_frozen = 100000000000.
+
+# SIMULATION BOX : for all space directions (in 2D & 3D use vector of doubles)
+cell_length = [1*L0]
+sim_length  = [20*L0]
+
+# ELECTROMAGNETIC BOUNDARY CONDITIONS
+# bc_em_type_x : two strings, x boundary conditions for EM fields 
+# bc_em_type_y : two strings, y boundary conditions for EM fields 
+#                'periodic'      : periodic BC (using MPI topology)
+#                'silver-muller' : injecting/absorbing
+bc_em_type_x  = ["periodic"]
+
+
+# RANDOM seed used to randomize the random number generator
+random_seed = 0
+
+# EXTERNAL FIELDS
+ExtField(
+	field = ["Ex"],
+	profile = 0.001
+)
+
+# DEFINE ALL SPECIES
+# species_type       = string, given name to the species (e.g. ion, electron, positron, test ...)
+# initPosition_type  = string, "regular" or "random"
+# initMomentum_type  = string "cold", "maxwell-juettner" or "rectangular"
+# c_part_max         = float, factor on the memory reserved for the total number of particles
+# mass               = float, particle mass in units of the electron mass
+# dynamics_type      = string, type of species dynamics = "norm" or "rrLL"
+# time_frozen        = float, time during which particles are frozen in units of the normalization time
+# radiating          = boolean, if true, incoherent radiation calculated using the Larmor formula 
+# n_part_per_cell    = integer or function, number of particles/cell
+# charge             = float or function, particle charge in units of the electron charge
+# charge_density     = float or function, species charge density in units of the "critical" density
+#     or nb_density for number density
+# mean_velocity      = list of floats or functions, mean velocity in units of the speed of light
+# temperature        = list of floats or functions, temperature in units of m_e c^2
+# Predefined functions: constant, trapezoidal, gaussian, polygonal, cosine
+
+Species(
+	species_type = "copper1",
+	initPosition_type = "regular",
+	initMomentum_type = "maxwell-juettner",
+	n_part_per_cell = 50000,
+	mass = 115845.,      # =  mass of Cu atom
+	charge = 17.,
+	charge_density = constant(1268.),
+	mean_velocity = [0., 0., 0.],
+	temperature = [0.0006], # 300 eV
+	time_frozen = 0.0,
+	bc_part_type_west = "none",
+	bc_part_type_east = "none"
+)
+Species(
+	species_type = "electron1",
+	initPosition_type = "regular",
+	initMomentum_type = "maxwell-juettner",
+	n_part_per_cell= 100000,
+	mass = 1.0,
+	charge = -1.0,
+	charge_density = constant(1268.),
+	mean_velocity = [0., 0., 0.],
+	temperature = [0.0006], # 300 eV
+	time_frozen = 0.0,
+	bc_part_type_west = "none",
+	bc_part_type_east = "none"
+)
+
+Species(
+	species_type = "copper2",
+	initPosition_type = "regular",
+	initMomentum_type = "maxwell-juettner",
+	n_part_per_cell = 50000,
+	mass = 115845.,      # =  mass of Cu atom
+	charge = 25.,
+	charge_density = constant(1869.),
+	mean_velocity = [0., 0., 0.],
+	temperature = [0.002], # 1000 eV
+	time_frozen = 0.0,
+	bc_part_type_west = "none",
+	bc_part_type_east = "none"
+)
+Species(
+	species_type = "electron2",
+	initPosition_type = "regular",
+	initMomentum_type = "maxwell-juettner",
+	n_part_per_cell= 100000,
+	mass = 1.0,
+	charge = -1.0,
+	charge_density = constant(1869.),
+	mean_velocity = [0., 0., 0.],
+	temperature = [0.002], # 1000 eV
+	time_frozen = 0.0,
+	bc_part_type_west = "none",
+	bc_part_type_east = "none"
+)
+
+# COLLISIONS
+# species1    = list of strings, the names of the first species that collide
+# species2    = list of strings, the names of the second species that collide
+#               (can be the same as species1)
+# coulomb_log = float, Coulomb logarithm. If negative or zero, then automatically computed.
+Collisions(
+	species1 = ["copper1"],
+	species2 = ["electron1"],
+	coulomb_log = 2.
+)
+Collisions(
+	species1 = ["copper2"],
+	species2 = ["electron2"],
+	coulomb_log = 2.
+)
+
+# ---------------------
+# DIAGNOSTIC PARAMETERS
+# ---------------------
+
+# print_every (on screen text output) 
+print_every = 10
+
+# DIAGNOSTICS ON FIELDS
+fieldDump_every    = 5
+avgfieldDump_every = 5
+ntime_step_avg     = 1
+
+
+# DIAGNOSTICS ON SCALARS
+# every = integer, number of time-steps between each output
+DiagScalar(
+	every = 1
+)
+
+
+# DIAGNOSTICS ON PARTICLES - project the particles on a N-D arbitrary grid
+# ------------------------------------------------------------------------
+# output       = string: "density", "charge_density" or "jx_density"
+#                parameter that describes what quantity is obtained 
+# every        = integer > 0: number of time-steps between each output
+# time_average = integer > 0: number of time-steps to average
+# species      = list of strings, one or several species whose data will be used
+# axes         = list of axes
+# Each axis is a list: [_type_,_min_,_max_,_nsteps_,"logscale","edge_inclusive"]
+#   _type_ is a string, one of the following options:
+#      x, y, z, px, py, pz, p, gamma, ekin, vx, vy, vz, v or charge
+#   The data is discretized for _type_ between _min_ and _max_, in _nsteps_ bins
+#   The optional "logscale" sets the scale to logarithmic
+#   The optional "edge_inclusive" forces the particles that are outside (_min_,_max_)
+#     to be counted in the extrema bins
+#   Example : axes = ("x", 0, 1, 30)
+#   Example : axes = ("px", -1, 1, 100, "edge_inclusive")
+
+DiagParticles(
+	output = "jx_density",
+	every = 5,
+	time_average = 4,
+	species = ["electron1"],
+	axes = [
+		 ["x",  0, 20*L0, 1]
+	]
+)
+DiagParticles(
+	output = "jx_density",
+	every = 5,
+	time_average = 4,
+	species = ["electron2"],
+	axes = [
+		 ["x",  0, 20*L0, 1]
+	]
+)
+
+
+DiagParticles(
+	output = "density",
+	every = 5,
+	time_average = 4,
+	species = ["electron1"],
+	axes = [
+		 ["x",  0, 20*L0, 1]
+	]
+)
+DiagParticles(
+	output = "density",
+	every = 5,
+	time_average = 4,
+	species = ["electron2"],
+	axes = [
+		 ["x",  0, 20*L0, 1]
+	]
+)
+
---
+++ benchmarks/collisions/ionization_stopping_power.py
@@ -0,0 +1,120 @@
+execfile("../../scripts/Diagnostics.py")
+
+import numpy as np
+import matplotlib.pyplot as plt
+ln = np.log
+
+def RohrlichStoppingPower(Z, E): # MeV*cm^2
+	# Formula of Rohrlich&Carlson (1954)
+	#  taken from thesis of F Perez
+	g = E/511.+1.
+	v2 = 1.-g**-2
+	I0 = ( 9.76*Z + 58.8*Z**-0.19 )/1000.
+	return 2.55e-25*Z/(1.-g**-2) * (
+		ln((g+1.)/2.*(E/I0)**2)
+		+(1.-(2.*g-1.)*ln(2.) + (g-1.)**2/8.)/g**2  )
+
+
+
+plt.figure(1,figsize=(8,3.5))
+
+# First, slowing down for a few examples
+########################################
+D = []
+sims = ["2","3","4"]
+for sim in sims:
+	S=Smilei("ionization_stopping_power"+sim)
+	
+	D.append(S.ParticleDiagnostic("#0/#1",slice={"x":"all"},units=["fs"],
+		linestyle="None", marker='o', markersize=4, markeredgewidth=0., skipAnimation=True))
+multiPlot(*D)
+fig = plt.gcf()
+ax = plt.gca()
+ax.xaxis.labelpad = 0
+ax.yaxis.labelpad = 0
+ax.set_xscale("log")
+ax.set_yscale("log")
+ax.set_xlim(10.,1e6)
+ax.set_ylim(1e-2,3.)
+ax.set_position([0.1, 0.19, 0.35,  0.77])
+
+# Theory
+for sim in sims:
+	S=Smilei("ionization_stopping_power"+sim)
+	
+	Zi = np.double(S.namelist.Species["ion1"].atomic_number)
+	ni = np.double(S.namelist.Species["ion1"].nb_density) * 1.11e21 # cm^-3
+	Ee = S.namelist.E * 511. # electron energy in keV
+	vel = S.namelist.vel * 3e10 # electron velocity in cm/s
+	dt = 1e-15 * S.namelist.timestep*4. # in s
+	t = 0.
+	energy = []
+	time = []
+	for k in range(30000):
+		time.append(t)
+		energy.append(Ee)
+		Ee -= ni*vel*dt*RohrlichStoppingPower(Zi, Ee)*1e3
+		if Ee<0.: break
+		vel = np.sqrt(1.-1./(1.+Ee/511.)**2)*3e10
+		t += dt
+	momentum = np.sqrt((1.+np.array(energy)/511.)**2-1.)
+	ax.plot(np.array(time)*1e15, momentum)
+
+# Make nicer plot
+plt.plot([0],"sk", markersize=4, markeredgewidth=0., label="Smilei")
+plt.plot([0],"-k",label="Rohrlich & Carlson")
+plt.legend(loc="best", prop={'size':10})
+ax.set_ylabel("Momentum / ($m_e c$)")
+ax.set_title("")
+
+
+
+## Second, stopping power as a function of initial energy
+#########################################################
+S=Smilei("ionization_stopping_power1")
+
+wavelength_SI = np.double(S.namelist.wavelength_SI)
+timestep = np.double(S.namelist.timestep)
+
+# get ion stuff
+ni = np.double(S.namelist.Species["ion1"].nb_density)
+mass = np.double(S.namelist.Species["ion1"].mass)*9.11e-28 # g
+rho = mass*ni*1.1e21 # g/cm^3
+Zi = np.double(S.namelist.Species["ion1"].atomic_number)
+
+
+# Loop electron species
+npoints = S.namelist.npoints
+energy = []
+Qsmilei = []
+for i in range(npoints):
+	print "electron"+str(i)
+	# get electron velocity
+	ve = np.double(S.namelist.Species["electron"+str(i)].mean_velocity[0])
+	Ee = (1./np.sqrt(1.-ve**2)-1.)*511. # energy in keV
+	energy.append(Ee)
+	
+	# Get data
+	D = S.ParticleDiagnostic("#"+str(2*i)+"/#"+str(2*i+1)+"",slice={"x":"all"})
+	P = np.array(D.getData()) # momentum
+	# stopping power in MeV/cm
+	diffP = -(P[-1]-P[0])/(D.times[-1]-D.times[0]) * (0.511*2.*np.pi*1e-2/wavelength_SI/timestep)
+	# stopping power in MeV*cm2/g
+	Qsmilei.append( diffP/rho )
+	
+# Plot simulation result
+ax = fig.add_axes([0.57, 0.19, 0.37,  0.77])
+ax.loglog(energy, Qsmilei, 'ok', label="Smilei", markersize=4)
+# Plot theory
+E = np.logspace(-1,10,1000)
+ax.loglog(E, RohrlichStoppingPower(Zi,E)/mass,'-k', label="Rohrlich & Carlson")
+# Make nicer plot
+ax.set_ylim(1.,3e2)
+fig.set_facecolor("w")
+plt.legend(loc="best", prop={'size':10})
+ax.xaxis.labelpad = 0
+ax.yaxis.labelpad = 0
+ax.set_xlabel("Incident electron energy (keV)")
+ax.set_ylabel("Stopping power (Mev cm$^2$/g)")
+ax.set_xticks([1e-2, 1e0, 1e2, 1e4, 1e6, 1e8, 1e10])
+
