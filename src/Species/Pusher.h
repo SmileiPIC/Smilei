43bc863f7cf1d00731ecdbebea1fa074e7b91a40
Mickael Grech <mickael.grech@gmail.com>
2016-03-21 22:03:23 +0100

    Corrected my error in removing int nDim_ in pusher.h

--- src/Species/Pusher.h
+++ src/Species/Pusher.h
@@ -22,11 +22,12 @@ public:
     virtual void operator() (Particles &particles, int ipart, LocalFields Epart, LocalFields Bpart, double& gf) = 0;
 
 protected:
-    double dt, dts2,dx;
+    double dt, dts2;
     //! \todo Move mass_ in Particles_
     // mass_ relative to Species but used in the particle pusher
     double mass_;
     double one_over_mass_;
+    int nDim_;
 
 };//END class
 
