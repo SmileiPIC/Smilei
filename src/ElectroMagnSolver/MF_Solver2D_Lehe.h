652cb91ba493c7c1d03f2264e61de83fca6aa99e
Anna Grassi <a.grassi8@gmail.com>
2016-03-21 21:12:28 +0100

    Added Lehe Maxwell-Faraday solver

---
+++ src/ElectroMagnSolver/MF_Solver2D_Lehe.h
@@ -0,0 +1,34 @@
+#ifndef MF_SOLVER2D_LEHE_H
+#define MF_SOLVER2D_LEHE_H
+
+#include "Solver2D.h" 
+class ElectroMagn;
+
+//  --------------------------------------------------------------------------------------------------------------------
+//! Class Pusher
+//  --------------------------------------------------------------------------------------------------------------------
+class MF_Solver2D_Lehe : public Solver2D
+{
+
+public:
+    //! Creator for MF_Solver2D_Lehe
+    MF_Solver2D_Lehe(Params &params);
+    virtual ~MF_Solver2D_Lehe();
+
+    //! Overloading of () operator
+    virtual void operator()( ElectroMagn* fields);
+ 
+    // Parameters for the Maxwell-Faraday solver
+    double dx;
+    double dy;
+    double delta_x;
+    double beta_x;
+    double beta_y;
+    double Beta_x;
+    double Beta_y;
+
+protected:
+
+};//END class
+
+#endif
