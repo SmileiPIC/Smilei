5344299ec1140acc0b87ab772f3ef6c3a01c3304
jderouil <julien.derouillat@cea.fr>
2016-07-01 19:03:32 +0200

    Added DiagnosticFields3D files

---
+++ src/Diagnostic/DiagnosticFields3D.h
@@ -0,0 +1,32 @@
+
+#ifndef DIAGNOSTICFIELDS3D_H
+#define DIAGNOSTICFIELDS3D_H
+
+#include <string>
+#include <vector>
+
+#include "DiagnosticFields.h"
+#include "Tools.h"
+
+class DiagnosticFields3D : public DiagnosticFields {
+public:
+    DiagnosticFields3D( Params &params, SmileiMPI* smpi, Patch* patch, int );
+    ~DiagnosticFields3D();
+    
+    void setFileSplitting( SmileiMPI* smpi, VectorPatch& vecPatches ) override;
+    
+    //! Copy patch field to current "data" buffer
+    void getField( Patch* patch, int ) override;
+    
+    void writeField(hid_t, int) override;
+
+private:
+    
+    // Tools for re-reading and re-writing the file in a folded pattern
+    hid_t filespace_reread, filespace_firstwrite, memspace_reread, memspace_firstwrite;
+    std::vector<double> data_reread, data_rewrite;
+    unsigned int rewrite_npatch, rewrite_xmin, rewrite_ymin, rewrite_zmin, rewrite_npatchx, rewrite_npatchy, rewrite_npatchz;
+    std::vector<unsigned int> rewrite_patches_x, rewrite_patches_y, rewrite_patches_z;
+};
+
+#endif
