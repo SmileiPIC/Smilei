64da9632105b1231ee2c7028c5fe98e5fd7363ec
Fred Perez <frederic.perez@polytechnique.edu>
2016-09-13 14:15:24 +0200

    Better parsing of command-line namelist parts

--- smilei.sh
+++ smilei.sh
@@ -7,16 +7,16 @@ DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd ) # dir of this script
 smilei=$DIR/smilei # path to the smilei executable
 script=$0 # name of this script
 
-# Function to which check that there are arguments remaining
-check () {
-  if [ "$#" -lt 1 ]; then
-    echo "usage: $script [proc numbers] namelist [outdir]"
-    exit 1
-  fi
+# Function to explain usage
+usage () {
+    echo "usage: $script [number_of_procs] namelist1 [namelist2 ...] [-o outdir]"
 }
 
 # Check that at least one argument provided
-check $@
+if [ "$#" -lt 1 ]; then
+    usage
+    exit 1
+fi
 
 # If first argument is a number, this is the number of procs
 if [[ $1 =~ ^[0-9]+$ ]]; then
@@ -26,19 +26,54 @@ else
     proc=1
 fi
 
-# Check there are arguments remaining
-check $@
+# Loop additional namelists
+namelists=()
+namelist_files=()
+firstnamelist=""
+while [[ $# -ge 1 ]]; do
+    arg=$1
+    # If -o option, leave
+    if [[ $arg == -o* ]]; then
+        break
+    fi
+    # Try to convert to absolute path
+    absnml="$(cd "$(dirname "$nml")" && pwd)/$(basename "$arg")"
+    # If file, then add it as namelist
+    if [ -f $absnml ]; then
+        namelists+=("`basename $absnml`")
+        namelist_files+=("$absnml")
+        # Save the first namelist
+        if [ -z $firstnamelist ]; then
+            firstnamelist=$absnml
+        fi
+    # Otherwise add it as python commands
+    else
+        namelists+=("\"$arg\"")
+    fi
+    shift
+done
 
-# Next argument is the path to the namelist
-nml=$1
-base="`basename $nml .py`" # strip directory and extension
-shift
+# Error if no namelist
+if [ -z $firstnamelist ]; then
+    echo "$script: At least one namelist must be provided."
+    usage
+    exit 1
+fi
 
 # Next argument, if exists, is the outdir 
-outdir=`dirname $nml`/$base # by default, the outdir is same as namelist
+base="`basename $firstnamelist .py`" # strip directory and extension of first namelist
+outdir=`dirname $firstnamelist`/$base # by default, the outdir is same as first namelist
+# otherwise, provided as argument
 if [ "$#" -gt 0 ]; then
-  outdir=$1/$base # otherwise, provided as argument
-  shift
+    if [[ $1 == -o ]]; then
+        shift
+        if [ "$#" -lt 1 ]; then
+            usage
+            exit 1
+        fi
+        outdir=$1
+        shift
+    fi
 fi
 
 # If outdir already exist, error
@@ -49,10 +84,19 @@ if [ -d $outdir ]; then
   exit 1
 fi
 
+# Error if arguments leftover
+if [ "$#" -gt 0 ]; then
+    usage
+    exit 1
+fi
+
 # Make new directory, go there, and run
 mkdir -p $outdir
-cp $nml $outdir
+# copy namelists there
+for namelist in "${namelist_files[@]}"; do
+    cp $namelist $outdir
+done
 cd $outdir
-$MPIEXEC -mca btl tcp,sm,self -np $proc $smilei `basename $nml`
+$MPIEXEC -mca btl tcp,sm,self -np $proc $smilei "${namelists[@]}"
 cd $H
 
