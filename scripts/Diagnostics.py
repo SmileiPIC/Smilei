ea6a426e16d629e4bdb8e80354dbc4c15965584a
Fred Perez <frederic.perez@polytechnique.edu>
2016-09-16 16:17:09 +0200

    Fix backwards compatibility for Diagnostics.py

--- scripts/Diagnostics.py
+++ scripts/Diagnostics.py
@@ -1,4 +1,5 @@
 
 # For backwards compatibility, this scripts imports the python module
 import os
-exec(open(os.path.dirname(os.path.abspath(__file__))+os.sep+'PythonModule'+os.sep+'Smilei'+os.sep+'__init__.py','r').read())
+import inspect
+exec(open(os.path.dirname(os.path.abspath(inspect.stack()[0][1]))+os.sep+'PythonModule'+os.sep+'Smilei'+os.sep+'__init__.py','r').read())
