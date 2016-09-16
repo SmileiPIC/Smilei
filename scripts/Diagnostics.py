f926a5122d4bd193803e6d5a3197b9a8ae697760
Fred Perez <frederic.perez@polytechnique.edu>
2016-09-16 17:24:30 +0200

    Even better compatibility

--- scripts/Diagnostics.py
+++ scripts/Diagnostics.py
@@ -1,5 +1,14 @@
 
-# For backwards compatibility, this scripts imports the python module
+# For backwards compatibility, this scripts imports Smilei's python module
+#  located in scripts/PythonModule
+
 import os
 import inspect
-exec(open(os.path.dirname(os.path.abspath(inspect.stack()[0][1]))+os.sep+'PythonModule'+os.sep+'Smilei'+os.sep+'__init__.py','r').read())
+this_script_dir = os.path.dirname(os.path.abspath(inspect.stack()[0][1]))
+s = os.sep
+module_file = this_script_dir+s+'PythonModule'+s+'Smilei'+s+'__init__.py'
+if not os.path.isfile(module_file):
+	print("The Diagnostics.py file cannot be open this way.")
+	print("Use the function `execfile`, or the `%run` command in ipython.")
+else:
+	exec(open(module_file,'r').read())
