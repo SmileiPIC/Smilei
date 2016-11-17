dcd9d8f460b7590a92dd2e59cf800c0032eba84d
tommaso vinci <tommaso.vinci@polytechnique.edu>
2016-11-17 15:18:39 +0100

    revert DiagnosticFields1D writing

--- src/Diagnostic/DiagnosticFields1D.cpp
+++ src/Diagnostic/DiagnosticFields1D.cpp
@@ -17,12 +17,12 @@ DiagnosticFields1D::DiagnosticFields1D( Params &params, SmileiMPI* smpi, Patch* 
     
     // Calculate the patch size
     patch_size.resize(1);
-    patch_size[0] = params.n_space[0]+1;
+    patch_size[0] = params.n_space[0];
     total_patch_size = patch_size[0];
     
     // define space in file
     hsize_t global_size[1];
-    global_size[0] = tot_number_of_patches * total_patch_size ;
+    global_size[0] = tot_number_of_patches * total_patch_size + 1;
     filespace = H5Screate_simple(1, global_size, NULL);
     memspace  = H5Screate_simple(1, global_size, NULL ); // redefined later
 
@@ -65,10 +65,15 @@ void DiagnosticFields1D::getField( Patch* patch, unsigned int field_index )
         field = static_cast<Field1D*>(patch->EMfields->allFields    [field_index]);
     }
     // Copy field to the "data" buffer
-    unsigned int ix = patch_offset_in_grid[0] ;
-    unsigned int iout = patch_size[0] * (patch->Hindex()-refHindex);
-    memcpy(&data[iout], &(*field)(ix), patch_size[0]*sizeof(double)); 
-
+    unsigned int ix = patch_offset_in_grid[0] - (patch->hindex==0 ? 1:0);
+    unsigned int ix_max = ix + patch_size[0] + (patch->hindex==0 ? 1:0);
+    unsigned int iout = total_patch_size * (patch->Hindex()-refHindex) + ((refHindex==0 && !patch->hindex==0)?1:0);
+    while( ix < ix_max ) {
+        data[iout] = (*field)(ix);
+        ix++;
+        iout++;
+    }
+    
     if( time_average>1 ) field->put_to(0.0);
 }
 
