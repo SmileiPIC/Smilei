56bd43a18ff783e9eb1a9ea56a6af418fdc6842f
Julien <julien.derouillat@cea.fr>
2016-11-25 11:58:03 +0100

    Replaced std::array by std::vector for BG-Q

--- src/SmileiMPI/AsyncMPIbuffers.h
+++ src/SmileiMPI/AsyncMPIbuffers.h
@@ -16,9 +16,9 @@ public:
     virtual void allocate(int nDim_field);
     
     //! ndim vectors of 2 sent requests (1 per direction) 
-    std::vector< std::array<MPI_Request, 2> > srequest;
+    std::vector< std::vector<MPI_Request> > srequest;
     //! ndim vectors of 2 received requests (1 per direction) 
-    std::vector< std::array<MPI_Request, 2> > rrequest;
+    std::vector< std::vector<MPI_Request> > rrequest;
 };
 
 class SpeciesMPIbuffers : public AsyncMPIbuffers {
@@ -29,18 +29,18 @@ public:
     void allocate(int nDim_field) override;
 
     //! ndim vectors of 2 sent packets of particles (1 per direction) 
-    std::vector< std::array<Particles, 2> > partRecv;
+    std::vector< std::vector<Particles > > partRecv;
     //! ndim vectors of 2 received packets of particles (1 per direction) 
-    std::vector< std::array<Particles, 2> > partSend;
+    std::vector< std::vector<Particles > > partSend;
 
     //! ndim vectors of 2 vectors of index particles to send (1 per direction) 
     //!   - not sent
     //    - used to sort Species::indexes_of_particles_to_exchange built in Species::dynamics
-    std::vector< std::array< std::vector<int>, 2> > part_index_send;
+    std::vector< std::vector< std::vector<int> > > part_index_send;
     //! ndim vectors of 2 numbers of particles to send (1 per direction) 
-    std::vector< std::array< int, 2> > part_index_send_sz;
+    std::vector< std::vector< int > > part_index_send_sz;
     //! ndim vectors of 2 numbers of particles to receive (1 per direction) 
-    std::vector< std::array< int, 2> > part_index_recv_sz;
+    std::vector< std::vector< int > > part_index_recv_sz;
 
 };
 
