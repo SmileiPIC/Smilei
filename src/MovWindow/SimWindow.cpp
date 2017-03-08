SmileiPIC/Smilei

commit 9c8440df497942b5b3c0e6949ff459d1d0dd3f2f

Fix particles creation for real new patches when moving window is applied and in case of restart

src/MovWindow/SimWindow.cpp
@@ -138,6 +138,12 @@ void SimWindow::operate(VectorPatch& vecPatches, SmileiMPI* smpi, Params& params
         if (mypatch->MPI_neighbor_[0][1] != MPI_PROC_NULL){
             smpi->recv( mypatch, mypatch->MPI_neighbor_[0][1], (mypatch->hindex)*nmessage, params );
         }
+        else { // Must force particles creation, see in SpeciesFactory :
+            // if (params.restart)
+            //     thisSpecies->particles->initialize( 0, params.nDim_particle );
+            for (unsigned int ispec=0 ; ispec<nSpecies ; ispec++)
+                mypatch->vecSpecies[ispec]->createParticles(params.n_space, params, mypatch, 0 );
+        }
         mypatch->EMfields->laserDisabled();
         vecPatches.patches_[patch_to_be_created[j]] = mypatch ;
     }

commit 47e47f14906fbde4cc61bddfbbfa6cc1decf0e72

Fix bug introduced in 9c8440df

src/MovWindow/SimWindow.cpp
@@ -141,8 +141,9 @@ void SimWindow::operate(VectorPatch& vecPatches, SmileiMPI* smpi, Params& params
         else { // Must force particles creation, see in SpeciesFactory :
             // if (params.restart)
             //     thisSpecies->particles->initialize( 0, params.nDim_particle );
-            for (unsigned int ispec=0 ; ispec<nSpecies ; ispec++)
-                mypatch->vecSpecies[ispec]->createParticles(params.n_space, params, mypatch, 0 );
+            if (params.restart)
+                for (unsigned int ispec=0 ; ispec<nSpecies ; ispec++)
+                    mypatch->vecSpecies[ispec]->createParticles(params.n_space, params, mypatch, 0 );
         }
         mypatch->EMfields->laserDisabled();
         vecPatches.patches_[patch_to_be_created[j]] = mypatch ;