3c8d7b418c377b23ad7dc8c22ca56f8f755cef6e
Fred Perez <frederic.perez@polytechnique.edu>
2017-03-30 16:01:28 +0200

    Some new references

---
+++ validation/validate_tst1d_2_two_str_instability.py
@@ -0,0 +1,14 @@
+import os, re, numpy as np
+from Smilei import *
+
+S = Smilei(".", verbose=False)
+
+some_particles_x = S.TrackParticles.ion(axes=["x"],select="any(t==0, x<0.02)").getData()["x"][0]
+Validate("Regularly spaced particles", some_particles_x )
+
+momentum_distribution = S.ParticleDiagnostic.Diag0(timesteps=0, slice={"x":"all"}).getData()[0]
+Validate("Electron momentum distribution", momentum_distribution, 1e-7 )
+
+itimes = S.ParticleDiagnostic.Diag0().getAvailableTimesteps()
+Validate("Timesteps in particle diagnostic", itimes )
+
