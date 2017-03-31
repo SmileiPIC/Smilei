fbf98ebb83354679d7125af2de6a7e1fd33f24ed
mathieu lobet <mathieu.lobet@gmail.com>
2017-03-31 16:25:42 +0200

    Creation of the pusher Higuera Cary

--- src/Species/PusherFactory.h
+++ src/Species/PusherFactory.h
@@ -5,6 +5,7 @@
 #include "PusherBoris.h"
 #include "PusherBorisNR.h"
 #include "PusherRRLL.h"
+#include "PusherHigueraCary.h"
 
 #include "Params.h"
 #include "Species.h"
@@ -37,6 +38,10 @@ public:
         else if ( species->dynamics_type == "rrll" )
         {
             Push = new PusherRRLL( params, species );
+        }
+        else if ( species->dynamics_type == "higueracary" )
+        {
+            Push = new PusherHigueraCary( params, species );
         }
         else {
             ERROR( "For species " << species->species_type << ": unknown dynamics_type `" << species->dynamics_type << "`");
