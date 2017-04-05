cc4e0ae697766ca4c7a2365eae32c32352d7e8db
Fred Perez <frederic.perez@polytechnique.edu>
2017-04-05 19:54:57 +0200

    try again tst1d_2

--- validation/validate_tst1d_2_two_str_instability.py
+++ validation/validate_tst1d_2_two_str_instability.py
@@ -6,11 +6,11 @@ S = Smilei(".", verbose=False)
 some_particles_x = S.TrackParticles.ion(axes=["x"],select="any(t==0, x<0.02)").getData()["x"][0]
 Validate("Regularly spaced particles", some_particles_x, 1e-7)
 
-momentum_distribution = S.ParticleDiagnostic.Diag0(timesteps=0, slice={"x":"all"}).getData()[0]
-Validate("Electron momentum distribution", momentum_distribution, 1e-7 )
-
 itimes = S.ParticleDiagnostic.Diag0().getAvailableTimesteps()
 Validate("Timesteps in particle diagnostic", itimes )
 
+px = S.ParticleDiagnostic.Diag0(slice={"x":"all"}, timesteps=0   ).getData()[0]
+Validate("Initial electron momentum distribution", px, 0.1 )
+
 px = S.ParticleDiagnostic.Diag0(slice={"x":"all"}, timesteps=2000).getData()[0]
-Validate("Electron momentum distribution", px, 0.1)
+Validate("Final electron momentum distribution", px, 0.1)
