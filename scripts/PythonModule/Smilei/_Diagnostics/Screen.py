defa4ae326612ff4d373db58268aa8c85259366b
Fred Perez <frederic.perez@polytechnique.edu>
2017-04-20 18:35:39 +0200

    Fix post-processing of 4D particle diagnostics

--- scripts/PythonModule/Smilei/_Diagnostics/Screen.py
+++ scripts/PythonModule/Smilei/_Diagnostics/Screen.py
@@ -296,7 +296,7 @@ class Screen(Diagnostic):
 			self._bsize = plot_diff[0]
 		else:
 			self._bsize = self._np.prod( self._np.array( self._np.meshgrid( *plot_diff ) ), axis=0)
-			self._bsize = self._bsize.transpose(range(1,len(plot_diff))+[0])
+			self._bsize = self._bsize.transpose([1,0]+range(2,len(plot_diff)))
 		self._bsize = cell_volume / self._bsize
 		if not hasComposite: self._bsize *= coeff
 		self._bsize = self._np.squeeze(self._bsize)
