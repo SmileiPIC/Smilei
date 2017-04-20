6610d8384890220f35c7f9a06645f3bd5b98e39b
Fred Perez <frederic.perez@polytechnique.edu>
2017-04-20 15:29:42 +0200

    Started validation of scalars

--- validation/validate_tst1d_4_radiation_pressure_acc.py
+++ validation/validate_tst1d_4_radiation_pressure_acc.py
@@ -3,9 +3,30 @@ from Smilei import *
 
 S = Smilei(".", verbose=False)
 
+
+# PHYSICS OF ION ACCELERATION
 ion_profiles = S.Field.Field0.Rho_eon().getData()
 ion_front = []
 for p in ion_profiles: ion_front += [np.flatnonzero(p<-10)[0]]
 ion_front = np.array(ion_front)
 Validate("Ion front position vs time", ion_front, 100.)
+
+
+# SCALARS RELATED TO GLOBAL ENERGIES
+Validate("Scalar Utot"     , S.Scalar.Utot     ().getData(), 200.)
+Validate("Scalar Ukin"     , S.Scalar.Ukin     ().getData(), 100.)
+Validate("Scalar Uelm"     , S.Scalar.Uelm     ().getData(), 100.)
+Validate("Scalar Uexp"     , S.Scalar.Uexp     ().getData(), 200.)
+Validate("Scalar Ubal"     , S.Scalar.Ubal     ().getData(), 1.  )
+Validate("Scalar Ubal_norm", S.Scalar.Ubal_norm().getData(), 0.005)
+Validate("Scalar Uelm_Ex"  , S.Scalar.Uelm_Ex  ().getData(), 100.)
+Validate("Scalar Uelm_Ey"  , S.Scalar.Uelm_Ey  ().getData(), 100.)
+Validate("Scalar Uelm_Ez"  , S.Scalar.Uelm_Ez  ().getData(), 100.)
+Validate("Scalar Uelm_Bx_m", S.Scalar.Uelm_Bx_m().getData(), 100.)
+Validate("Scalar Uelm_By_m", S.Scalar.Uelm_By_m().getData(), 100.)
+Validate("Scalar Uelm_Bz_m", S.Scalar.Uelm_Bz_m().getData(), 100.)
+
+# SCALARS RELATED TO SPECIES
+Validate("Scalar Ukin_ion" , S.Scalar.Ukin_ion ().getData(), 100.)
+
 
