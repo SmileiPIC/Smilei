8594ee6b4f187e05a2284ae3f2fcdf308f3e982e
Mickael Grech <mickael.grech@gmail.com>
2017-09-13 17:22:52 +0200

    Lehe Solver: cleanup of the coefficient names (remove insanity by Anna ;)

--- src/ElectroMagnSolver/MF_Solver2D_Lehe.cpp
+++ src/ElectroMagnSolver/MF_Solver2D_Lehe.cpp
@@ -12,11 +12,13 @@ MF_Solver2D_Lehe::MF_Solver2D_Lehe(Params &params)
     //ERROR("Under development, not yet working");
     dx = params.cell_length[0];
     dy = params.cell_length[1];
+    
+    beta_yx = 1./8.;
+    beta_xy = pow(dx/dy,2)/8.;
     delta_x = (1./4.)*(1.-pow( sin(M_PI*dt_ov_dx/2.)/dt_ov_dx,2 ) );
-    beta_x =   1./8.;
-    beta_y =   pow(dx/dy,2)/8.;
-    Beta_x =  1.-2.*beta_x;
-    Beta_y =  1.-2.*beta_y-3.*delta_x;
+    
+    alpha_y =  1.-2.*beta_yx;
+    alpha_x =  1.-2.*beta_xy-3.*delta_x;
 }
 
 MF_Solver2D_Lehe::~MF_Solver2D_Lehe()
@@ -34,29 +36,30 @@ void MF_Solver2D_Lehe::operator() ( ElectroMagn* fields )
     Field2D* Bz2D = static_cast<Field2D*>(fields->Bz_);
 
 
-
-//    for (unsigned int i=0 ; i<nx_p;  i++) {
-//    for (unsigned int i=1 ; i<nx_d-1;  i++) {
-    for (unsigned int i=1 ; i<nx_d-2;  i++) {
+    // Magnetic field Bx^(p,d)
+    for (unsigned int i=1 ; i<nx_p-1;  i++) {
         for (unsigned int j=1 ; j<ny_d-1 ; j++) {
-            (*Bx2D)(i,j) -= dt_ov_dy * ( Beta_x*((*Ez2D)(i,j) - (*Ez2D)(i,j-1)) +beta_x*( (*Ez2D)(i+1,j)- (*Ez2D)(i+1,j-1) +(*Ez2D)(i-1,j)- (*Ez2D)(i-1,j-1)));
+            (*Bx2D)(i,j) -= dt_ov_dy * (  alpha_y * ((*Ez2D)(i,j) - (*Ez2D)(i,j-1))
+                          + beta_yx * ( (*Ez2D)(i+1,j)-(*Ez2D)(i+1,j-1) + (*Ez2D)(i-1,j)-(*Ez2D)(i-1,j-1))  );
         }
     }
     
     // Magnetic field By^(d,p)
     for (unsigned int i=2 ; i<nx_d-2 ; i++) {
         for (unsigned int j=1 ; j<ny_p-1 ; j++) {
-            (*By2D)(i,j) += dt_ov_dx * ( Beta_y*((*Ez2D)(i,j) - (*Ez2D)(i-1,j)) +beta_y*((*Ez2D)(i,j+1)- (*Ez2D)(i-1,j+1) +(*Ez2D)(i,j-1)- (*Ez2D)(i-1,j-1) ) +delta_x*( (*Ez2D)(i+1,j) - (*Ez2D)(i-2,j)));
+            (*By2D)(i,j) += dt_ov_dx * (  alpha_x * ((*Ez2D)(i,j) - (*Ez2D)(i-1,j))
+                          + beta_xy * ( (*Ez2D)(i,j+1)-(*Ez2D)(i-1,j+1) + (*Ez2D)(i,j-1)-(*Ez2D)(i-1,j-1) )
+                          + delta_x * ( (*Ez2D)(i+1,j) - (*Ez2D)(i-2,j)));
         }
-        //(*By2D)(i,0) += dt_ov_dx *( (1-2*beta_y)*((*Ez2D)(i,0) - (*Ez2D)(i-1,0)) +beta_y*((*Ez2D)(i,1)- (*Ez2D)(i-1,1) +(*Ez2D)(i,ny_p-1)- (*Ez2D)(i-1,ny_p-1) ) );
-        //(*By2D)(i,ny_p-1) += dt_ov_dx * ( (1-2*beta_y)*((*Ez2D)(i,ny_p-1) - (*Ez2D)(i-1,ny_p-1)) +beta_y*((*Ez2D)(i,0)- (*Ez2D)(i-1,0) +(*Ez2D)(i,ny_p-2)- (*Ez2D)(i-1,ny_p-2) ) );
     
     
     // Magnetic field Bz^(d,d)
-    //for (unsigned int i=1 ; i<nx_d-1 ; i++) {
         for (unsigned int j=1 ; j<ny_d-1 ; j++) {
-            (*Bz2D)(i,j) += dt_ov_dy * (Beta_x*( (*Ex2D)(i,j) - (*Ex2D)(i,j-1) ) +beta_x*((*Ex2D)(i+1,j)- (*Ex2D)(i+1,j-1)+ (*Ex2D)(i-1,j)- (*Ex2D)(i-1,j-1)))
-            -               dt_ov_dx * (Beta_y*( (*Ey2D)(i,j) - (*Ey2D)(i-1,j) ) +beta_y*((*Ey2D)(i,j+1)- (*Ey2D)(i-1,j+1)+ (*Ey2D)(i,j-1)- (*Ey2D)(i-1,j-1)) +delta_x*( (*Ey2D)(i+1,j) - (*Ey2D)(i-2,j)));
+            (*Bz2D)(i,j) += dt_ov_dy * (  alpha_y * ((*Ex2D)(i,j)-(*Ex2D)(i,j-1))
+                          + beta_yx * ( (*Ex2D)(i+1,j)-(*Ex2D)(i+1,j-1) + (*Ex2D)(i-1,j)-(*Ex2D)(i-1,j-1))  )
+                          - dt_ov_dx * (  alpha_x * ((*Ey2D)(i,j)-(*Ey2D)(i-1,j))
+                          + beta_xy * ( (*Ey2D)(i,j+1)-(*Ey2D)(i-1,j+1) + (*Ey2D)(i,j-1)-(*Ey2D)(i-1,j-1) )
+                          + delta_x * ( (*Ey2D)(i+1,j)-(*Ey2D)(i-2,j) ) );
         }
     }
     
