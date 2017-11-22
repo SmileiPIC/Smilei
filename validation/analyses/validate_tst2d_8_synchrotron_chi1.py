db6a40b3013738d2976d62376398f190c60e99a0
Mathieu_Lobet <mathieu.lobet@gmail.com>
2017-11-22 11:56:20 +0100

    Update validation script

--- validation/analyses/validate_tst2d_8_synchrotron_chi1.py
+++ validation/analyses/validate_tst2d_8_synchrotron_chi1.py
@@ -17,15 +17,14 @@ import happi
 
 S = happi.Open(["./restart*"], verbose=False)
 
-
-
 # List of relativistic pushers
 radiation_list = ["cont","disc"]
 
 ukin_dict = {}
 urad_dict = {}
 
-# We load successively the particle track associated
+# ______________________________________________________________________________
+# We load successively particle scalar diagnostics associated
 # to each radiation algorithm
 for radiation in radiation_list:
 
@@ -58,10 +57,34 @@ for radiation in radiation_list:
 urad_rel_err = abs(urad_dict["disc"] - urad_dict["cont"]) / urad_dict["cont"].max()
 ukin_rel_err = abs(ukin_dict["disc"] - ukin_dict["cont"]) / ukin_dict["cont"][0]
 
-print ' Comparision continuous/discontinuous methods'
-print ' Maximum relative error kinetic energy',ukin_rel_err.max()
-print ' Maximum relative error radiative energy',urad_rel_err.max()
+print(' Comparision continuous/discontinuous methods')
+print(' Maximum relative error kinetic energy: {}'.format(ukin_rel_err.max()))
+print(' Maximum relative error radiative energy: {}'.format(urad_rel_err.max()))
 
 # Validation difference between continuous and discontinuous methods
 Validate("Relative error on the kinetic energy / ukin at t=0: " , ukin_rel_err.max(), 0.02 )
 Validate("Relative error on the radiative energy / urad max " , urad_rel_err.max(), 0.02 )
+
+# ______________________________________________________________________________
+# Checking of the particle binning
+
+# Loop over the timesteps
+for timestep in range(0,5000,500):
+
+    print("Timestep: {}".format(timestep))
+
+    # Loop over the species/radiation models
+    for i,radiation in enumerate(radiation_list):
+        print("Species {}:".format(radiation))
+        # Weight
+        weight_diag = S.ParticleBinning(diagNumber=i,timesteps=timestep).get()
+        weight = np.array(weight_diag["data"])
+        # Weight x chi
+        weight_chi_diag = S.ParticleBinning(diagNumber=i+len(radiation_list),timesteps=timestep).get()
+        weight_chi = np.array(weight_chi_diag["data"])
+        # Local average chi
+        chi = weight_chi[weight>0] / weight[weight>0]
+        # Maximal chi value
+        chi_max = chi.max()
+
+        print(" - Maximal chi value: {}".format(chi_max))
