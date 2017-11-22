5985861dcfd053844eff0d007eecee0f0239d643
Arnaud Beck <beck@llr.in2p3.fr>
2017-11-22 18:20:54 +0100

    finished stencil

--- src/ElectroMagnSolver/MF_Solver3D_Lehe.cpp
+++ src/ElectroMagnSolver/MF_Solver3D_Lehe.cpp
@@ -41,13 +41,11 @@ void MF_Solver3D_Lehe::operator() ( ElectroMagn* fields )
     for (unsigned int i=1 ; i<nx_p-1;  i++) {
         for (unsigned int j=1 ; j<ny_d-1 ; j++) {
             for (unsigned int k=1 ; j<nz_d-1 ; k++) {
-                (*Bx3D)(i,j,k) += -dt_ov_dy * (  
-                                                 alpha_y * ( (*Ez3D)(i,  j,k)  - (*Ez3D)(i,  j-1,k)                                      )
+                (*Bx3D)(i,j,k) += -dt_ov_dy * (  alpha_y * ( (*Ez3D)(i,  j,k)  - (*Ez3D)(i,  j-1,k)                                      )
                                                + beta_yx * ( (*Ez3D)(i+1,j,k)  - (*Ez3D)(i+1,j-1,k) + (*Ez3D)(i-1,j,k)-(*Ez3D)(i-1,j-1,k))
                                               )
-                                  +dt_ov_dz * ( 
-                                                  alpha_y * (1.)
-                                                + beta_zx * (1.)
+                                  +dt_ov_dz * ( /*alpha_z*/ alpha_y * ( (*Ey3D)(i  ,j  ,k)-(*Ey3D)(i  ,j,k-1))                                         
+                                              +/*beta_zx*/ beta_yx * ( (*Ey3D)(i+1,j  ,k)-(*Ey3D)(i+1,j,k-1) + (*Ey3D)(i-1,j  ,k)-(*Ey3D)(i-1,j,k-1)) 
                                               );
             }
         }
@@ -57,24 +55,28 @@ void MF_Solver3D_Lehe::operator() ( ElectroMagn* fields )
     for (unsigned int i=2 ; i<nx_d-2 ; i++) {
         for (unsigned int j=1 ; j<ny_p-1 ; j++) {
             for (unsigned int k=1 ; j<nz_d-1 ; k++) {
-                (*By3D)(i,j,k) += dt_ov_dx * (  alpha_x * ( (*Ez3D)(i,j,k) - (*Ez3D)(i-1,j,k))
-                                              + beta_xy * ( (*Ez3D)(i,j+1,k)-(*Ez3D)(i-1,j+1,k) + (*Ez3D)(i,j-1,k)-(*Ez3D)(i-1,j-1,k) )
-                                              + delta_x * ( (*Ez3D)(i+1,j,k) - (*Ez3D)(i-2,j,k)))
-                                 -dt_ov_dz * (  alpha_y * ()
-                                              + beta_zx * ()
+                (*By3D)(i,j,k) += dt_ov_dx * (  alpha_x * ( (*Ez3D)(i,  j  ,k  ) - (*Ez3D)(i-1,j  ,k  ))
+                                              + beta_xy * ( (*Ez3D)(i,  j+1,k  ) - (*Ez3D)(i-1,j+1,k  ) + (*Ez3D)(i  ,j-1,k  )-(*Ez3D)(i-1,j-1,k) )
+                                              + beta_xz * ( (*Ez3D)(i  ,j  ,k+1) - (*Ez3D)(i-1,j  ,k+1) + (*Ez3D)(i  ,j  ,k-1)-(*Ez3D)(i-1,j  ,k-1))
+                                              + delta_x * ( (*Ez3D)(i+1,j  ,k  ) - (*Ez3D)(i-2,j  ,k)))
+                                 -dt_ov_dz * ( /*alpha_z*/ alpha_y * ( (*Ex3D)(i  ,j  ,k)-(*Ex3D)(i  ,j,k-1))                                         
+                                              +/*beta_zx*/ beta_yx * ( (*Ex3D)(i+1,j  ,k)-(*Ex3D)(i+1,j,k-1) + (*Ex3D)(i-1,j  ,k)-(*Ex3D)(i-1,j,k-1)) 
                                              ); 
             }
         }
+    }
     
     
     // Magnetic field Bz^(d,d,p)
+    for (unsigned int i=2 ; i<nx_d-2 ; i++) {
         for (unsigned int j=1 ; j<ny_d-1 ; j++) {
             for (unsigned int k=1 ; j<nz_p-1 ; k++) {
-                (*Bz3D)(i,j,k) += dt_ov_dy * (  alpha_y * ( (*Ex3D)(i  ,j  ,k)-(*Ex3D)(i  ,j-1,k))
-                                              + beta_yx * ( (*Ex3D)(i+1,j  ,k)-(*Ex3D)(i+1,j-1,k) + (*Ex3D)(i-1,j  ,k)-(*Ex3D)(i-1,j-1,k))  )
-                                - dt_ov_dx * (  alpha_x * ( (*Ey3D)(i  ,j  ,k)-(*Ey3D)(i-1,j  ,k))
-                                              + beta_xy * ( (*Ey3D)(i  ,j+1,k)-(*Ey3D)(i-1,j+1,k) + (*Ey3D)(i  ,j-1,k)-(*Ey3D)(i-1,j-1,k))
-                                              + delta_x * ( (*Ey3D)(i+1,j  ,k)-(*Ey3D)(i-2,j  ,k) ) );
+                (*Bz3D)(i,j,k) += dt_ov_dy * (  alpha_y * ( (*Ex3D)(i  ,j  ,k  )-(*Ex3D)(i    ,j-1,k))
+                                              + beta_yx * ( (*Ex3D)(i+1,j  ,k  )-(*Ex3D)(i+1  ,j-1,k)   + (*Ex3D)(i-1,j  ,k  )-(*Ex3D)(i-1,j-1,k  )))
+                                - dt_ov_dx * (  alpha_x * ( (*Ey3D)(i  ,j  ,k  )-(*Ey3D)(i-1  ,j  ,k))
+                                              + beta_xy * ( (*Ey3D)(i  ,j+1,k  )-(*Ey3D)(i-1  ,j+1,k)   + (*Ey3D)(i  ,j-1,k  )-(*Ey3D)(i-1,j-1,k  ))
+                                              + beta_xz * ( (*Ey3D)(i  ,j  ,k+1)-(*Ey3D)(i-1  ,j  ,k+1) + (*Ey3D)(i  ,j  ,k-1)-(*Ey3D)(i-1,j  ,k-1))
+                                              + delta_x * ( (*Ey3D)(i+1,j  ,k  )-(*Ey3D)(i-2  ,j  ,k) ) );
             }
         }
     }
