6e4c41af7f2d855509eabed8568ed8f544c29556
izemzemi <izemzemi@llr.in2p3.fr>
2017-11-23 16:53:40 +0100

    correction

--- src/ElectroMagnSolver/SolverRZ.h
+++ src/ElectroMagnSolver/SolverRZ.h
@@ -12,14 +12,15 @@ class SolverRZ : public Solver
 public:
     //! Creator for Solver
     SolverRZ(Params &params) : Solver(params) {
-	nx_p = params.n_space[0]+1+2*params.oversize[0];
-	nx_d = params.n_space[0]+2+2*params.oversize[0];
-	ny_p = params.n_space[1]+1+2*params.oversize[1];
-	ny_d = params.n_space[1]+2+2*params.oversize[1];
+	nl_p = params.n_space[0]+1+2*params.oversize[0];
+	nl_d = params.n_space[0]+2+2*params.oversize[0];
+	nr_p = params.n_space[1]+1+2*params.oversize[1];
+	nr_d = params.n_space[1]+2+2*params.oversize[1];
 
     dt = params.timestep;
-	dt_ov_dx = params.timestep / params.cell_length[0];
-	dt_ov_dy = params.timestep / params.cell_length[1];
+	dr = params.cell_length[1];
+	dt_ov_dl = params.timestep / params.cell_length[0];
+	dt_ov_dr = params.timestep / params.cell_length[1];
 
     };
     virtual ~SolverRZ() {};
@@ -28,13 +29,14 @@ public:
     virtual void operator()( ElectroMagn* fields) = 0;
 
 protected:
-    unsigned int nx_p;
-    unsigned int nx_d;
-    unsigned int ny_p;
-    unsigned int ny_d;
+    unsigned int nl_p;
+    unsigned int nl_d;
+    unsigned int nr_p;
+    unsigned int nr_d;
     double dt;
-    double dt_ov_dx;
-    double dt_ov_dy;
+	double dr;
+    double dt_ov_dl;
+    double dt_ov_dr;
 
 };//END class
 
