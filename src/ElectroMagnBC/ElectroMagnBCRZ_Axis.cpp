46dcaf555190918a18cb567f19b7b3fed703eda1
izemzemi <izemzemi@llr.in2p3.fr>
2017-11-28 10:57:30 +0100

    Correction of boundary condition on axis

--- src/ElectroMagnBC/ElectroMagnBCRZ_Axis.cpp
+++ src/ElectroMagnBC/ElectroMagnBCRZ_Axis.cpp
@@ -10,7 +10,8 @@
 #include "ElectroMagn.h"
 #include "cField2D.h"
 #include "Tools.h"
-
+#include <complex>
+#include "dcomplex.h"
 using namespace std;
 
 ElectroMagnBCRZ_Axis::ElectroMagnBCRZ_Axis( Params &params, Patch* patch, unsigned int _min_max )
@@ -64,14 +65,66 @@ void ElectroMagnBCRZ_Axis::apply(ElectroMagn* EMfields, double time_dual, Patch*
     int imode = 0;
 
     // Static cast of the fields
-    cField2D* El2D = (static_cast<ElectroMagn3DRZ*>(EMfields))->El_[imode];
-    cField2D* Er2D = (static_cast<ElectroMagn3DRZ*>(EMfields))->Er_[imode];
-    cField2D* Et2D = (static_cast<ElectroMagn3DRZ*>(EMfields))->Et_[imode];
-    cField2D* Bl2D = (static_cast<ElectroMagn3DRZ*>(EMfields))->Bl_[imode];
-    cField2D* Br2D = (static_cast<ElectroMagn3DRZ*>(EMfields))->Br_[imode];
-    cField2D* Bt2D = (static_cast<ElectroMagn3DRZ*>(EMfields))->Bt_[imode];
- 
-    if (min_max == 2 && patch->isYmin() ) {
-    }
-    
+    cField2D* ElRZ = (static_cast<ElectroMagn3DRZ*>(EMfields))->El_[imode];
+    cField2D* ErRZ = (static_cast<ElectroMagn3DRZ*>(EMfields))->Er_[imode];
+    cField2D* EtRZ = (static_cast<ElectroMagn3DRZ*>(EMfields))->Et_[imode];
+    cField2D* BlRZ = (static_cast<ElectroMagn3DRZ*>(EMfields))->Bl_[imode];
+    cField2D* BrRZ = (static_cast<ElectroMagn3DRZ*>(EMfields))->Br_[imode];
+    cField2D* BtRZ = (static_cast<ElectroMagn3DRZ*>(EMfields))->Bt_[imode];
+	cField2D* BlRZ_old = (static_cast<ElectroMagn3DRZ*>(EMfields))->Bl_m[imode];
+    cField2D* BtRZ_old = (static_cast<ElectroMagn3DRZ*>(EMfields))->Bt_m[imode]; 
+	cField2D* JlRZ = (static_cast<ElectroMagn3DRZ*>(EMfields))->Jl_[imode];
+    cField2D* JrRZ = (static_cast<ElectroMagn3DRZ*>(EMfields))->Jr_[imode];
+    cField2D* JtRZ = (static_cast<ElectroMagn3DRZ*>(EMfields))->Jt_[imode];
+
+	if (min_max == 2 && patch->isYmin()){
+		if (imode==0){
+			//MF_Solver_Yee
+			for (unsigned int i=0 ; i<nl_d ; i++) {
+				(*BrRZ)(i,0)=0;
+			}
+			for (unsigned int i=0 ; i<nl_d ; i++) {
+				(*BtRZ)(i,0)= -(*BtRZ)(i,1);
+			}
+			for (unsigned int i=0 ; i<nl_p ; i++) {
+				(*BlRZ)(i,0)+= -(*BlRZ)(i,1)+(*BlRZ_old)(i,1)-4*dt_ov_dr*(*EtRZ)(i,1);
+			}
+			//MA_SolverRZ_norm
+			for (unsigned int i=0 ; i<nl_p ; i++) {
+				(*EtRZ)(i,0)=0;
+			}
+			for (unsigned int i=0 ; i<nl_p ; i++) {
+				(*ErRZ)(i,0)= -(*ErRZ)(i,1);
+			}
+			for (unsigned int i=0 ; i<nl_d ; i++) {
+				(*ElRZ)(i,0)+= 4*dt_ov_dr*(*BtRZ)(i,1)-dt*(*JlRZ)(i,0);
+			}
+		}
+		if (imode==1){
+			//MF
+			for (unsigned int i=0 ; i<nl_d ; i++) {
+				(*BlRZ)(i,0)= -(*BlRZ)(i,1);
+			}
+			for (unsigned int i=0 ; i<nl_d ; i++) {
+				(*BrRZ)(i,0)+= (*BrRZ)(i,1)+ Icpx*dt_ov_dr*(*ElRZ)(0,i)
+				+			dt_ov_dl*((*EtRZ)(i,0)-(*EtRZ)(i+1,0));
+			}
+			for (unsigned int i=0 ; i<nl_p ; i++) {
+				(*BtRZ)(i,0)+= -dt_ov_dl*((*ErRZ)(i+1,0)-(*ErRZ)(i,0)+(*ErRZ)(i+1,1)-(*ErRZ)(i,1))
+				+				2*dt_ov_dr*(*ElRZ)(i+1,1) - (*BtRZ_old)(i,1)+ (*BtRZ)(i,1);
+			}	
+			//MA
+			for (unsigned int i=0 ; i<nl_d ; i++) {
+				(*ElRZ)(i,0)= 0;
+			}
+			for (unsigned int i=0 ; i<nl_p ; i++) {
+				(*ErRZ)(i,0)= (*ErRZ)(i,1);
+			}
+			for (unsigned int i=0 ; i<nl_d ; i++) {
+				(*EtRZ)(i,0)= (*EtRZ)(i,1);
+			}	
+
+		}
+
+	}    
 }
