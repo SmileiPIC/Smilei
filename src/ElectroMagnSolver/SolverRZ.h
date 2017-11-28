dd0ded0953ab4fe93adf0145a76940eeb9728283
izemzemi <izemzemi@llr.in2p3.fr>
2017-11-28 10:59:36 +0100

    hh

--- src/ElectroMagnSolver/SolverRZ.h
+++ src/ElectroMagnSolver/SolverRZ.h
@@ -16,7 +16,7 @@ public:
 	nl_d = params.n_space[0]+2+2*params.oversize[0];
 	nr_p = params.n_space[1]+1+2*params.oversize[1];
 	nr_d = params.n_space[1]+2+2*params.oversize[1];
-
+	Nmode= params.Nmode;
     dt = params.timestep;
 	dr = params.cell_length[1];
 	dt_ov_dl = params.timestep / params.cell_length[0];
@@ -33,6 +33,7 @@ protected:
     unsigned int nl_d;
     unsigned int nr_p;
     unsigned int nr_d;
+	unsigned int Nmode;
     double dt;
 	double dr;
     double dt_ov_dl;
