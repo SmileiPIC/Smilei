1be560e3ad581384fa57bb887b930059dc2a1df4
haithem kallala <haithem.kallala@cea.fr>
2017-12-04 10:38:14 +0100

    trying to debug 2d spectral

--- src/ElectroMagnSolver/PXR_Solver2D_GPSTD.cpp
+++ src/ElectroMagnSolver/PXR_Solver2D_GPSTD.cpp
@@ -73,7 +73,7 @@ void PXR_Solver2D_GPSTD::operator() ( ElectroMagn* fields )
     duplicate_field_into_pxr( fields );
 
 #ifdef _PICSAR
-    picsar::push_psatd_ebfield_3d_();
+    picsar::push_psatd_ebfield_2d_();
 #else
     ERROR( "Smilei not linked with picsar" );
 #endif
