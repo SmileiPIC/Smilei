66f33bdca6e4b3f954f9041c5bf565b5e2bf7dc8
Arnaud Beck <beck@llr.in2p3.fr>
2017-12-13 15:05:00 +0100

    openPMD compatibility

--- src/Params/OpenPMDparams.cpp
+++ src/Params/OpenPMDparams.cpp
@@ -93,6 +93,9 @@ OpenPMDparams::OpenPMDparams(Params& p):
             } else if( params->EM_BCs[i][j] == "silver-muller" ) {
                 fieldBoundary          .addString( "open" );
                 fieldBoundaryParameters.addString( "silver-muller");
+            } else if( params->EM_BCs[i][j] == "buneman" ) {
+                fieldBoundary          .addString( "open" );
+                fieldBoundaryParameters.addString( "buneman");
             } else {
                 ERROR(" impossible boundary condition ");
             }
