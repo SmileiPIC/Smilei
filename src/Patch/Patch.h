57e76e3e7b0296cbe04098f8cfa4d9da0c64b560
FrancescoMassimo <ziof@Franks-MacBook-Pro.local>
2018-01-22 11:13:05 +0100

    added functions InitExchangeComplex

--- src/Patch/Patch.h
+++ src/Patch/Patch.h
@@ -113,12 +113,16 @@ public:
 
     //! init comm / exchange fields
     virtual void initExchange( Field* field ) = 0;
+    //! init comm / exchange complex fields
+    virtual void initExchangeComplex( Field* field ) = 0;
     //! finalize comm / exchange fields
     virtual void finalizeExchange( Field* field ) = 0;
     //! finalize comm / exchange complex fields
     virtual void finalizeExchangeComplex( Field* field ) = 0;
     //! init comm / exchange fields in direction iDim only
     virtual void initExchange( Field* field, int iDim ) = 0;
+    //! init comm / exchange complex fields in direction iDim only
+    virtual void initExchangeComplex( Field* field, int iDim ) = 0;
     //! finalize comm / exchange complex fields in direction iDim only
     virtual void finalizeExchangeComplex( Field* field, int iDim ) = 0;
 
