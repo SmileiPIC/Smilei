17d0b321d5b4d73b735f6d64007bde4f07b3c90b
Julien <julien.derouillat@cea.fr>
2018-01-24 10:43:41 +0100

    Move corner management in a new ElectroMagnBC3D class

--- src/ElectroMagnBC/ElectroMagnBC3D_SM.h
+++ src/ElectroMagnBC/ElectroMagnBC3D_SM.h
@@ -5,7 +5,7 @@
 
 #include <vector>
 #include "Tools.h"
-#include "ElectroMagnBC.h" 
+#include "ElectroMagnBC3D.h" 
 #include "ElectroMagn3D.h" 
 #include "Field3D.h"
 #include "Field2D.h"
@@ -15,7 +15,7 @@ class Params;
 class ElectroMagn;
 class Field;
 
-class ElectroMagnBC3D_SM : public ElectroMagnBC {
+class ElectroMagnBC3D_SM : public ElectroMagnBC3D {
 public:
 
     ElectroMagnBC3D_SM( Params &params, Patch* patch, unsigned int _min_max );
@@ -28,52 +28,6 @@ public:
     //! Save external fields for silver muller EM Boundary condition
     Field2D *Bx_val, *By_val, *Bz_val;
     
-private:
-    
-    //! Number of nodes on the primal grid in the x-direction
-    unsigned int nx_p;
-    
-    //! Number of nodes on the dual grid in the x-direction
-    unsigned int nx_d;
-    
-    //! Number of nodes on the primal grid in the y-direction
-    unsigned int ny_p;
-    
-    //! Number of nodes on the dual grid in the y-direction
-    unsigned int ny_d;
-    
-    //! Number of nodes on the primal grid in the y-direction
-    unsigned int nz_p;
-    
-    //! Number of nodes on the dual grid in the z-direction
-    unsigned int nz_d;
-    
-    //! Spatial step dx for 3D3V cartesian simulations
-    double dx;
-    
-    //! Spatial step dy for 3D3V cartesian simulations
-    double dy;
-
-    //! Spatial step dz for 3D3V cartesian simulations
-    double dz;
-
-    //! Ratio of the time-step by the spatial-step dt/dx for 3D3V cartesian simulations
-    double dt_ov_dx;
-    
-    //! Ratio of the time-step by the spatial-step dt/dy for 3D3V cartesian simulations
-    double dt_ov_dy;
-  
-    //! Ratio of the time-step by the spatial-step dt/dz for 3D3V cartesian simulations
-    double dt_ov_dz;
-  
-    //! Ratio of the spatial-step by the time-step dx/dt for 3D3V cartesian simulations
-    double dx_ov_dt;
-    
-    //! Ratio of the spatial-step by the time-step dy/dt for 3D3V cartesian simulations
-    double dy_ov_dt;
-    
-    //! Ratio of the spatial-step by the time-step dz/dt for 3D3V cartesian simulations
-    double dz_ov_dt;
 
 
 
