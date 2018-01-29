d8ff66a257e9200c271dbe7a0e2ff268d905ac24
Julien Derouillat <julien.derouillat@cea.fr>
2018-01-29 11:47:02 +0100

    Fix for complex MPI synchronization in RZ

--- src/Patch/PatchRZ.cpp
+++ src/Patch/PatchRZ.cpp
@@ -474,7 +474,7 @@ void PatchRZ::createType( Params& params )
             MPI_Type_commit( &(ntype_[0][ix_isPrim][iy_isPrim]) );
 
             ntype_[1][ix_isPrim][iy_isPrim] = MPI_DATATYPE_NULL;
-            MPI_Type_vector(2*nx, params.oversize[1], ny, 
+            MPI_Type_vector(nx, 2*params.oversize[1], 2*ny, 
                             MPI_DOUBLE, &(ntype_[1][ix_isPrim][iy_isPrim]));
             MPI_Type_commit( &(ntype_[1][ix_isPrim][iy_isPrim]) );
 
@@ -488,7 +488,7 @@ void PatchRZ::createType( Params& params )
             MPI_Type_commit( &(ntypeSum_[0][ix_isPrim][iy_isPrim]) );
             
             ntypeSum_[1][ix_isPrim][iy_isPrim] = MPI_DATATYPE_NULL;
-            MPI_Type_vector(2*nx, ny_sum, ny, 
+            MPI_Type_vector(nx, 2*ny_sum, 2*ny, 
                             MPI_DOUBLE, &(ntypeSum_[1][ix_isPrim][iy_isPrim]));
             MPI_Type_commit( &(ntypeSum_[1][ix_isPrim][iy_isPrim]) );
 
