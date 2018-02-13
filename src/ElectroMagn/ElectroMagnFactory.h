208b3e8fdf6744c5b7585ed988cf4804735f1a05
Arnaud Beck <beck@llr.in2p3.fr>
2018-02-13 14:53:11 +0100

    added 3DRZ geometry in electrmagn factory cloning

--- src/ElectroMagn/ElectroMagnFactory.h
+++ src/ElectroMagn/ElectroMagnFactory.h
@@ -157,6 +157,8 @@ public:
             newEMfields = new ElectroMagn2D(static_cast<ElectroMagn2D*>(EMfields), params, patch);
         } else if ( params.geometry == "3Dcartesian" ) {
             newEMfields = new ElectroMagn3D(static_cast<ElectroMagn3D*>(EMfields), params, patch);
+        } else if ( params.geometry == "3drz" ) {
+            newEMfields = new ElectroMagn3DRZ(static_cast<ElectroMagn3DRZ*>(EMfields), params, patch);
         }
         
         newEMfields->finishInitialization(vecSpecies.size(), patch);
