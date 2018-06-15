3a05ec21b3e9ab5402e004e9452db7bd753469af
FrancescoMassimo <ziof@Franks-MacBook-Pro-2.local>
2018-06-15 22:49:59 +0200

    tentative addiction of BCs on ponderomotive potential

--- src/ElectroMagnBC/EnvelopeBC3D_refl.cpp
+++ src/ElectroMagnBC/EnvelopeBC3D_refl.cpp
@@ -42,18 +42,23 @@ void EnvelopeBC3D_refl::apply(LaserEnvelope* envelope, double time_dual, Patch* 
 {
   
     // Static cast of the field    
-    cField3D* A3D = static_cast<cField3D*>(envelope->A_);
+    cField3D* A3D        = static_cast<cField3D*>(envelope->A_);     // the envelope at timestep n
+    cField3D* A03D       = static_cast<cField3D*>(envelope->A0_);    // the envelope at timestep n-1
+    Field3D*  Phi3D      = static_cast<Field3D*>(envelope->Phi_);    // the ponderomotive potential Phi=|A|^2/2 at timestep n
+    Field3D*  Phiold3D   = static_cast<Field3D*>(envelope->Phiold_); // the ponderomotive potential Phi=|A|^2/2 at timestep n-1
   
     // APPLICATION OF BCs OVER THE FULL GHOST CELL REGION
   
     if (min_max == 0 && patch->isXmin() ) {
         
-        // FORCE CONSTANT ENVELOPE FIELD
+        // FORCE CONSTANT ENVELOPE FIELD ON BORDER
         
         for (unsigned int i=oversize_; i>0; i--) {
             for (unsigned int j=0 ; j<ny_p ; j++) {
                 for (unsigned int k=0 ; k<nz_p ; k++) {
-                  (*A3D)(i-1,j,k) = (*A3D)(i,j,k);
+                  (*A3D)     (i-1,j,k) = (*A3D)(i,j,k);
+                  (*Phi3D)   (i-1,j,k) = std::abs((*A3D)  (i,j,k)) * std::abs((*A3D)  (i,j,k)) * 0.5;
+                  (*Phiold3D)(i-1,j,k) = std::abs((*A03D) (i,j,k)) * std::abs((*A03D) (i,j,k)) * 0.5;
                 }//k
             }//j
         }//i
@@ -61,12 +66,14 @@ void EnvelopeBC3D_refl::apply(LaserEnvelope* envelope, double time_dual, Patch* 
     }
     else if (min_max == 1 && patch->isXmax() ) {
             
-        // FORCE CONSTANT ENVELOPE FIELD
+        // FORCE CONSTANT ENVELOPE FIELD ON BORDER
                 
         for (unsigned int i=nx_p-oversize_; i<nx_p; i++) {
             for (unsigned int j=0 ; j<ny_p ; j++) {
                 for (unsigned int k=0 ; k<nz_p ; k++) {
-                  (*A3D)(i,j,k) = (*A3D)(i-1,j,k);
+                  (*A3D)     (i,j,k) = (*A3D)(i-1,j,k);
+                  (*Phi3D)   (i,j,k) = std::abs((*A3D)  (i-1,j,k)) * std::abs((*A3D)  (i-1,j,k)) * 0.5;
+                  (*Phiold3D)(i,j,k) = std::abs((*A03D) (i-1,j,k)) * std::abs((*A03D) (i-1,j,k)) * 0.5;
                 }//k
             }//j
         }//i
@@ -74,12 +81,14 @@ void EnvelopeBC3D_refl::apply(LaserEnvelope* envelope, double time_dual, Patch* 
     }
     else if (min_max == 2 && patch->isYmin() ) {
         
-        // FORCE CONSTANT ENVELOPE FIELD
+        // FORCE CONSTANT ENVELOPE FIELD ON BORDER
         
         for (unsigned int i=0; i<nx_p; i++) {
             for (unsigned int j=oversize_ ; j>0 ; j--) {
                 for (unsigned int k=0; k<nz_p; k++) {
-                    (*A3D)(i,j-1,k) = (*A3D)(i,j,k);
+                    (*A3D)     (i,j-1,k) = (*A3D)(i,j,k);
+                    (*Phi3D)   (i,j-1,k) = std::abs((*A3D)  (i,j,k)) * std::abs((*A3D)  (i,j,k)) * 0.5;
+                    (*Phiold3D)(i,j-1,k) = std::abs((*A03D) (i,j,k)) * std::abs((*A03D) (i,j,k)) * 0.5;
                 }//k
             }//j
         }//i
@@ -87,12 +96,14 @@ void EnvelopeBC3D_refl::apply(LaserEnvelope* envelope, double time_dual, Patch* 
     }
     else if (min_max == 3 && patch->isYmax() ) {
       
-        // FORCE CONSTANT ENVELOPE FIELD
+        // FORCE CONSTANT ENVELOPE FIELD ON BORDER
         
         for (unsigned int i=0; i<nx_p; i++) {
             for (unsigned int j=ny_p-oversize_; j<ny_p ; j++) {
                 for (unsigned int k=0; k<nz_p; k++) {
-                  (*A3D)(i,j,k) = (*A3D)(i,j-1,k);
+                  (*A3D)     (i,j,k) = (*A3D)(i,j-1,k);
+                  (*Phi3D)   (i,j,k) = std::abs((*A3D)  (i,j-1,k)) * std::abs((*A3D)  (i,j-1,k)) * 0.5;
+                  (*Phiold3D)(i,j,k) = std::abs((*A03D) (i,j-1,k)) * std::abs((*A03D) (i,j-1,k)) * 0.5;
                 }//k
             }//j
         }//i
@@ -100,12 +111,14 @@ void EnvelopeBC3D_refl::apply(LaserEnvelope* envelope, double time_dual, Patch* 
     }
     else if (min_max == 4 && patch->isZmin() ) {
         
-        // FORCE CONSTANT ENVELOPE FIELD
+        // FORCE CONSTANT ENVELOPE FIELD ON BORDER
         
         for (unsigned int i=0; i<nx_p; i++) {
             for (unsigned int j=0; j<ny_p; j++) {
                 for (unsigned int k=oversize_; k>0; k--){
-                  (*A3D)(i,j,k-1) = (*A3D)(i,j,k);
+                  (*A3D)     (i,j,k-1) = (*A3D)(i,j,k);
+                  (*Phi3D)   (i,j,k-1) = std::abs((*A3D)  (i,j,k)) * std::abs((*A3D)  (i,j,k)) * 0.5;
+                  (*Phiold3D)(i,j,k-1) = std::abs((*A03D) (i,j,k)) * std::abs((*A03D) (i,j,k)) * 0.5;
                 }//k
             }//j
         }//i
@@ -113,12 +126,14 @@ void EnvelopeBC3D_refl::apply(LaserEnvelope* envelope, double time_dual, Patch* 
     }
     else if (min_max == 5 && patch->isZmax() ) {
       
-        // FORCE CONSTANT ENVELOPE FIELD
+        // FORCE CONSTANT ENVELOPE FIELD ON BORDER
         
         for (unsigned int i=0; i<nx_p; i++) {
             for (unsigned int j=0; j<ny_p; j++) {
                 for (unsigned int k=nz_p-oversize_; k<nz_p; k++) {
-                  (*A3D)(i,j,k) = (*A3D)(i,j,k-1);
+                  (*A3D)     (i,j,k) = (*A3D)(i,j,k-1);
+                  (*Phi3D)   (i,j,k) = std::abs((*A3D)  (i,j,k-1)) * std::abs((*A3D)  (i,j,k-1)) * 0.5;
+                  (*Phiold3D)(i,j,k) = std::abs((*A03D) (i,j,k-1)) * std::abs((*A03D) (i,j,k-1)) * 0.5;
                 }//z
             }//j
         }//i
