60515c806dbf363d82e48735854ee1ed7ad57e15
izemzemi <izemzemi@llr.in2p3.fr>
2018-07-13 17:33:05 +0200

    addition sign missing in J

--- src/Projector/ProjectorRZ2Order.cpp
+++ src/Projector/ProjectorRZ2Order.cpp
@@ -287,7 +287,7 @@ void ProjectorRZ2Order::operator() (complex<double>* Jl, complex<double>* Jr, co
     }
      e_delta_inv =1./e_delta;
     //defining crt_p 
-     complex<double> crt_p = - charge_weight*Icpx/(2*M_PI*e_bar*dt*imode);   
+     complex<double> crt_p = - charge_weight*Icpx/(e_bar*dt*imode);   
     for (unsigned int i=0; i < 5; i++) {
         DSx[i] = Sx1[i] - Sx0[i];
         DSy[i] = Sy1[i] - Sy0[i];
@@ -668,7 +668,7 @@ void ProjectorRZ2Order::operator() (complex<double>* Jl, complex<double>* Jr, co
     }
      e_delta_inv =1./e_delta;
     //defining crt_p 
-    complex<double> crt_p = -charge_weight*Icpx/(2*M_PI*dt*imode*e_bar);
+    complex<double> crt_p = -charge_weight*Icpx/(dt*imode*e_bar);
     for (unsigned int i=0; i < 5; i++) {
         DSx[i] = Sx1[i] - Sx0[i];
         DSy[i] = Sy1[i] - Sy0[i];
@@ -734,7 +734,7 @@ void ProjectorRZ2Order::operator() (complex<double>* Jl, complex<double>* Jr, co
             jloc = j+jpo;
             linindex = iloc*b_dim[1]+jloc;
             if (jloc+ j_domain_begin == 0){
-                Jl [linindex] =  C_m * Jx_p[i][j]*6. /dr ; // iloc = (i+ipo)*b_dim[1];
+                Jl [linindex] +=  C_m * Jx_p[i][j]*6. /dr ; // iloc = (i+ipo)*b_dim[1];
                 //Jl [linindex+1] += Jl [linindex-1];
                 //Jl [linindex+2] += Jl [linindex-2];
                 }
@@ -787,7 +787,7 @@ void ProjectorRZ2Order::operator() (complex<double>* Jl, complex<double>* Jr, co
                 rho [linindex] += C_m*charge_weight* Sx1[i]*Sy1[j]/abs((jloc+ j_domain_begin)*dr); // iloc = (i+ipo)*b_dim[1];
                 }
             else {
-                rho [linindex] =  C_m*charge_weight* Sx1[i]*Sy1[j]*6./dr; // iloc = (i+ipo)*b_dim[1];
+                rho [linindex] +=  C_m*charge_weight* Sx1[i]*Sy1[j]*6./dr; // iloc = (i+ipo)*b_dim[1];
                 //rho [linindex+1] += rho [linindex-1];
                 //rho [linindex+2] += rho [linindex-2];
                 }
