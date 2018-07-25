b2e7ebdd7773f873dbaa7720e51210da01471fd4
Julien <julien.derouillat@cea.fr>
2018-07-25 09:27:48 +0200

    Set grid size of 3D Silver Muller bounday conditions in function of picsar

--- src/ElectroMagnBC/ElectroMagnBC3D.cpp
+++ src/ElectroMagnBC/ElectroMagnBC3D.cpp
@@ -16,13 +16,13 @@ ElectroMagnBC3D::ElectroMagnBC3D( Params &params, Patch* patch, unsigned int _mi
 
     // number of nodes of the primal and dual grid in the x-direction
     nx_p = n_space[0]+1+2*params.oversize[0];
-    nx_d = nx_p+1;
+    nx_d = nx_p+1-params.is_pxr;
     // number of nodes of the primal and dual grid in the y-direction
     ny_p = n_space[1]+1+2*params.oversize[1];
-    ny_d = ny_p+1;
+    ny_d = ny_p+1-params.is_pxr;
     // number of nodes of the primal and dual grid in the z-direction
     nz_p = n_space[2]+1+2*params.oversize[2];
-    nz_d = nz_p+1;
+    nz_d = nz_p+1-params.is_pxr;
     
     // spatial-step and ratios time-step by spatial-step & spatial-step by time-step (in the x-direction)
     dx       = params.cell_length[0];
