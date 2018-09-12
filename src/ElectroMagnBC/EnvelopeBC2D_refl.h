c7899ba0eaaeb99b7fa926c23d5e8c2a2d70c42d
Z10F <ziof@Franks-MacBook-Pro-5.local>
2018-09-12 15:02:39 +0200

    BC for envelope in 2D

---
+++ src/ElectroMagnBC/EnvelopeBC2D_refl.h
@@ -0,0 +1,47 @@
+
+#ifndef ENVELOPEBC2D_refl_H
+#define ENVELOPEBC2D_refl_H
+
+
+#include <vector>
+#include "Tools.h"
+#include "EnvelopeBC.h" 
+#include "LaserEnvelope.h" 
+#include "Field2D.h"
+#include "cField2D.h"
+
+class Params;
+class LaserEnvelope;
+//class Field;
+
+class EnvelopeBC2D_refl : public EnvelopeBC {
+public:
+
+    EnvelopeBC2D_refl( Params &params, Patch* patch, unsigned int _min_max );
+    ~EnvelopeBC2D_refl(){};
+
+    void apply(LaserEnvelope* envelope, double time_dual, Patch* patch) override;
+    
+    
+private:
+    
+    //! Oversize (nb of ghost cells)
+    unsigned int oversize_;
+    
+    //! Number of nodes on the primal grid in the x-direction
+    unsigned int nx_p;
+    
+    //! Number of nodes on the dual grid in the x-direction
+    unsigned int nx_d;
+    
+    //! Number of nodes on the primal grid in the y-direction
+    unsigned int ny_p;
+    
+    //! Number of nodes on the dual grid in the y-direction
+    unsigned int ny_d;
+  
+
+};
+
+#endif
+
