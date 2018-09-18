b9b5c0095c153363e94bf7e3f608caeeacdb8a05
Z10F <ziof@Franks-MacBook-Pro-8.local>
2018-09-18 21:12:07 +0200

    added error message for 2D envelope interpolation of order 4

--- src/Interpolator/Interpolator2D4Order.cpp
+++ src/Interpolator/Interpolator2D4Order.cpp
@@ -330,3 +330,21 @@ void Interpolator2D4Order::operator() (ElectroMagn* EMfields, Particles &particl
     }
 }
 
+
+void Interpolator2D4Order::interpolate_em_fields_and_envelope(ElectroMagn* EMfields, Particles &particles, SmileiMPI* smpi, int *istart, int *iend, int ithread, int ipart_ref)
+{
+   ERROR("Projection and interpolation for the envelope model are implemented only for interpolation_order = 2");
+}
+
+
+void Interpolator2D4Order::interpolate_envelope_and_old_envelope(ElectroMagn* EMfields, Particles &particles, SmileiMPI* smpi, int *istart, int *iend, int ithread, int ipart_ref)
+{
+   ERROR("Projection and interpolation for the envelope model are implemented only for interpolation_order = 2");
+}
+
+// probes like diagnostic !
+void Interpolator2D4Order::interpolate_envelope_and_susceptibility(ElectroMagn* EMfields, Particles &particles, int ipart, double* Env_A_abs_Loc, double* Env_Chi_Loc, double* Env_E_abs_Loc)
+{
+    ERROR("Projection and interpolation for the envelope model are implemented only for interpolation_order = 2");
+}
+
