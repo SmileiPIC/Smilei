385720179a0e088d722756eb72dc2aecc2f1a5b6
Frederic Perez <frederic.perez@polytechnique.edu>
2018-11-21 20:55:09 +0100

    more softening tst1d_10

--- validation/analyses/validate_tst1d_10_pair_electron_laser_collision.py
+++ validation/analyses/validate_tst1d_10_pair_electron_laser_collision.py
@@ -55,4 +55,4 @@ Validate("Radiated energy evolution: ", urad/ukin_electron[0], 3e-2 )
 
 Validate("Evolution of the number of electrons: ", ntot_electron, 120 )
 Validate("Evolution of the number of positrons: ", ntot_positron, 120 )
-Validate("Evolution of the number of photons: ", ntot_photon, 3000 )
+Validate("Evolution of the number of photons: ", ntot_photon, 6000 )
