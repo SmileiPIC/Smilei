1b7499ede049a2575081fc7dd55fe244676b4d72
Julien <julien.derouillat@cea.fr>
2019-03-15 11:36:04 +0100

    Bug in apply BC on fields grid (uncoupling feature AM)

---
+++ src/ElectroMagnBC/ElectroMagnBCAM.cpp
@@ -0,0 +1,43 @@
+
+#include "ElectroMagnBCAM.h"
+
+#include "Params.h"
+#include "Patch.h"
+#include "ElectroMagn.h"
+#include "cField2D.h"
+
+
+ElectroMagnBCAM::ElectroMagnBCAM( Params &params, Patch *patch, unsigned int _min_max )
+    : ElectroMagnBC( params, patch, _min_max )
+{
+    std::vector<unsigned int> n_space(params.n_space);
+    if (params.uncoupled_grids)
+        n_space = params.n_space_domain;
+
+    // number of nodes of the primal and dual grid in the l-direction
+    nl_p = n_space[0]+1+2*params.oversize[0];
+    nl_d = nl_p+1-params.is_pxr;
+    // number of nodes of the primal and dual grid in the r-direction
+    nr_p = n_space[1]+1+2*params.oversize[1];
+    nr_d = nr_p+1-params.is_pxr;
+    
+    // spatial-step and ratios time-step by spatial-step & spatial-step by time-step (in the l-direction)
+    dl       = params.cell_length[0];
+    dt_ov_dl = dt/dl;
+    dl_ov_dt = 1.0/dt_ov_dl;
+    
+    // spatial-step and ratios time-step by spatial-step & spatial-step by time-step (in the y-direction)
+    dr       = params.cell_length[1];
+    dt_ov_dr = dt/dr;
+    dr_ov_dt = 1.0/dt_ov_dr;
+    
+}
+
+ElectroMagnBCAM::~ElectroMagnBCAM()
+{
+}
+
+
+void ElectroMagnBCAM::applyBConEdges( ElectroMagn *EMfields, Patch *patch )
+{
+}
--- src/ElectroMagnBC/ElectroMagnBCAM_Axis.h
+++ src/ElectroMagnBC/ElectroMagnBCAM_Axis.h
@@ -5,7 +5,7 @@
 
 #include <vector>
 #include "Tools.h"
-#include "ElectroMagnBC.h"
+#include "ElectroMagnBCAM.h"
 #include "ElectroMagnAM.h"
 #include "cField2D.h"
 
@@ -14,7 +14,7 @@ class Params;
 class ElectroMagn;
 class Field;
 
-class ElectroMagnBCAM_Axis : public ElectroMagnBC
+class ElectroMagnBCAM_Axis : public ElectroMagnBCAM
 {
 public:
 
@@ -31,36 +31,6 @@ private:
 
     //! Conversion factor from degree to radian
     double conv_deg2rad;
-    
-    //! Number of nodes on the primal grid in the x-direction
-    unsigned int nl_p;
-    
-    //! Number of nodes on the dual grid in the x-direction
-    unsigned int nl_d;
-    
-    //! Number of nodes on the primal grid in the y-direction
-    unsigned int nr_p;
-    
-    //! Number of nodes on the dual grid in the y-direction
-    unsigned int nr_d;
-    
-    //! Spatial step dx for 2D3V cartesian simulations
-    double dl;
-    
-    //! Spatial step dy for 2D3V cartesian simulations
-    double dr;
-    
-    //! Ratio of the time-step by the spatial-step dt/dx for 2D3V cartesian simulations
-    double dt_ov_dl;
-    
-    //! Ratio of the time-step by the spatial-step dt/dy for 2D3V cartesian simulations
-    double dt_ov_dr;
-    
-    //! Ratio of the spatial-step by the time-step dx/dt for 2D3V cartesian simulations
-    double dr_ov_dt;
-    
-    //! Ratio of the spatial-step by the time-step dy/dt for 2D3V cartesian simulations
-    double dl_ov_dt;
     
     //! Number of modes
     unsigned int Nmode;
