aaf6b77c5887901fb92f30e9244ee7cc97509eb9
iltommi <tommaso.vinci@polytechnique.edu>
2019-07-04 17:18:46 +0200

    Update get-version.py
    using file ver if found

--- scripts/CompileTools/get-version.py
+++ scripts/CompileTools/get-version.py
@@ -5,12 +5,14 @@ proc1 = subprocess.Popen(["git describe --always"], stdout=subprocess.PIPE, stde
 proc2 = subprocess.Popen(["git rev-parse --abbrev-ref HEAD"], stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
 (out2, err2) = proc2.communicate()
 if err1 or err2:
-	if os.path.isfile(".version"):
+	f_name = os.path.join(os.path.dirname(os.path.realpath(__file__)), "ver")
+	if os.path.isfile(f_name):
 		sys.stderr.write("Version from .version file\n")
-		print(open('.version', 'r').read())
+		print(open(f_name, 'r').read())
 	else:
 		sys.stderr.write("Unknown smilei version\n")
 		print("??-??")
 else:
 	print(str(out1.strip())+"-"+str(out2.strip()))
+
 
