63f0a7687efb8e171ee11a5f97dcc529c16de295
Mathieu Lobet <mathieu.lobet@cea.fr>
2019-09-04 16:50:48 +0200

    Particle injection: - New benchmarks and validation for injection in 2d and 3d

---
+++ validation/analyses/validate_tst2d_14_particle_injection.py
@@ -0,0 +1,47 @@
+import os, re, numpy as np, math
+import happi
+
+S = happi.Open(["./restart*"], verbose=False)
+
+# Get some Smilei parameters
+timestep = S.namelist.Main.timestep
+simulation_time = S.namelist.Main.simulation_time
+
+diag_every = int(simulation_time / timestep)
+
+species_list = ["eon1", "pon1", "eon2", "pon2"]
+
+Scalar = {}
+
+for species in species_list:
+    name = "Ntot_{}".format(species)
+    Scalar[name] = np.array(S.Scalar(name).getData())
+    Validate("Scalar {}".format(name) , Scalar[name], 0.01)
+    
+    name = "Ukin_{}".format(species)
+    Scalar[name] = np.array(S.Scalar(name).getData())
+    Validate("Scalar {}".format(name) , Scalar[name], 0.01)
+
+    name = "Dens_{}".format(species)
+    Scalar[name] = np.array(S.Scalar(name).getData())
+    Validate("Scalar {}".format(name) , Scalar[name], 0.01)
+
+# Energy _________________________________________________________________
+
+for i in range(4):
+    particle_binning_initial = S.ParticleBinning(diagNumber=i,timesteps=0)
+    data_initial = np.array(particle_binning_initial.getData()[0])
+    
+    particle_binning = S.ParticleBinning(diagNumber=i,timesteps=diag_every)
+    data_final = np.array(particle_binning.getData()[0])
+    
+    Validate("Gamma spectrum for {}".format(species_list[i]) , data_final, 0.01)
+    
+    sum_initial = np.sum(data_initial)
+    sum_final = np.sum(data_final)
+    
+    error = (np.abs(np.subtract(data_initial / sum_initial, data_final / sum_final)))
+    
+    print(' Gamma spectrum max error for {}: {}'.format(species_list[i], np.max(error)))
+    
+    Validate("Gamma spectrum max error for {}".format(species_list[i]) , np.max(error), 0.01)
