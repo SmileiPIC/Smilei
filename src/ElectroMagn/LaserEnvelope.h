67870b02e614e2d9d118c0a28c68f120f950d6c7
Francesco Massimo <massimo@llr.in2p3.fr>
2019-09-05 19:10:05 +0200

    starting to implement the cylindrical envelope solver

--- src/ElectroMagn/LaserEnvelope.h
+++ src/ElectroMagn/LaserEnvelope.h
@@ -99,6 +99,21 @@ public:
     void centerPhi_and_GradPhi() override final;
 };
 
+// Class for envelope with cylindrical symmetry
+class LaserEnvelopeAM : public LaserEnvelope
+{
+public:
+    LaserEnvelopeAM( Params &params, Patch *patch, ElectroMagn *EMfields );
+    LaserEnvelopeAM( LaserEnvelope *envelope, Patch *patch, ElectroMagn *EMfields, Params &params, unsigned int n_moved );
+    void initEnvelope( Patch *patch, ElectroMagn *EMfields ) override final;
+    ~LaserEnvelopeAM();
+    void compute( ElectroMagn *EMfields ) override final;
+    void compute_Phi( ElectroMagn *EMfields ) override final;
+    void compute_gradient_Phi( ElectroMagn *EMfields ) override final;
+    void savePhi_and_GradPhi() override final;
+    void centerPhi_and_GradPhi() override final;
+};
+
 
 
 #endif
