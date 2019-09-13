fc1fb7c7da26e29f3488c5140c7622aea3f43be3
Francesco Massimo <massimo@llr.in2p3.fr>
2019-09-13 11:32:18 +0200

    add envelope fields in createField in AM

--- src/ElectroMagn/ElectroMagnAM.cpp
+++ src/ElectroMagn/ElectroMagnAM.cpp
@@ -1089,6 +1089,12 @@ Field *ElectroMagnAM::createField( string fieldname )
         return new cField2D( dimPrim, 2, false, fieldname );
     } else if( fieldname.substr( 0, 3 )=="Rho" ) {
         return new cField2D( dimPrim, fieldname );
+    } else if( fieldname.substr( 0, 9 )=="Env_A_abs" ) {
+        return new Field2D( dimPrim, 0, false, fieldname );
+    } else if( fieldname.substr( 0, 7 )=="Env_Chi" ) {
+        return new Field2D( dimPrim, 0, false, fieldname );
+    } else if( fieldname.substr( 0, 9 )=="Env_E_abs" ) {
+        return new Field2D( dimPrim, 0, false, fieldname );
     }
     
     ERROR( "Cannot create field "<<fieldname );
