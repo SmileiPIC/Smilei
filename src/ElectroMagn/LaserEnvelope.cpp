b589c224e208fb0e852828378813c91c8408970e
Julien <julien.derouillat@cea.fr>
2019-09-17 10:57:54 +0200

    Bug fix in AM laser envelope clone

--- src/ElectroMagn/LaserEnvelope.cpp
+++ src/ElectroMagn/LaserEnvelope.cpp
@@ -92,7 +92,7 @@ LaserEnvelope::LaserEnvelope( Params &params, Patch *patch, ElectroMagn *EMfield
 
 // Cloning constructor
 LaserEnvelope::LaserEnvelope( LaserEnvelope *envelope, Patch *patch, ElectroMagn *EMfields, Params &params, unsigned int n_moved ) :
-    cell_length( envelope->cell_length ), timestep( envelope->timestep ), i1_2k0_over_2dx( envelope->i1_2k0_over_2dx ),
+    cell_length( envelope->cell_length ), timestep( envelope->timestep ), i1_2k0_over_2dx( envelope->i1_2k0_over_2dx ), i1_2k0_over_2dl( envelope->i1_2k0_over_2dl ),
     one_plus_ik0dt( envelope->one_plus_ik0dt ), one_plus_ik0dt_ov_one_plus_k0sq_dtsq( envelope->one_plus_ik0dt_ov_one_plus_k0sq_dtsq )
 {
     if( n_moved ==0 ) {
