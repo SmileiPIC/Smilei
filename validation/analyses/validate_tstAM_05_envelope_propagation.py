56918a6ac0f09537cdf4525a712ea1cd083eb3e2
Francesco Massimo <massimo@llr.in2p3.fr>
2019-09-18 12:10:12 +0200

    added analyses for AM envelope benchmarks

---
+++ validation/analyses/validate_tstAM_05_envelope_propagation.py
@@ -0,0 +1,17 @@
+import os, re, numpy as np, math, h5py
+import happi
+
+S = happi.Open(["./restart*"], verbose=False)
+
+# 1-D PROBE IN AM
+Env_A_abs = S.Probe.Probe0.Env_A_abs(timesteps=300).getData()[0]
+Validate("1-D probe Env_A_abs at iteration 300", Env_A_abs, 0.01)
+
+# TEST THE GRID PARAMETERS
+with h5py.File("./restart000/Fields0.h5") as f:
+	dt = f["data/0000000000"].attrs["dt"]
+	dx = f["data/0000000000/Env_A_abs"].attrs["gridSpacing"]
+	patchSize = f["data/0000000000"].attrs["patchSize"]
+Validate("Value of the timestep" , dt, 1e-6)
+Validate("Value of the grid step", dx, 1e-6)
+Validate("Patch size", patchSize)
---
+++ validation/analyses/validate_tstAM_06_envelope_wake.py
@@ -0,0 +1,23 @@
+import os, re, numpy as np, math, h5py
+import happi
+
+S = happi.Open(["./restart*"], verbose=False)
+
+# 1-D PROBE IN AM
+Env_A_abs = S.Probe.Probe0.Env_A_abs(timesteps=300).getData()[0]
+Validate("1-D probe Env_A_abs at iteration 300", Env_A_abs, 0.01)
+
+Ex = S.Probe.Probe0.Ex(timesteps=300).getData()[0]
+Validate("1-D probe Ex at iteration 300", Ex, 0.01)
+
+Env_Chi = S.Probe.Probe0.Env_Chi(timesteps=300).getData()[0]
+Validate("1-D probe Env_Chi at iteration 300", Env_Chi, 0.01)
+
+# TEST THE GRID PARAMETERS
+with h5py.File("./restart000/Fields0.h5") as f:
+	dt = f["data/0000000000"].attrs["dt"]
+	dx = f["data/0000000000/Ex"].attrs["gridSpacing"]
+	patchSize = f["data/0000000000"].attrs["patchSize"]
+Validate("Value of the timestep" , dt, 1e-6)
+Validate("Value of the grid step", dx, 1e-6)
+Validate("Patch size", patchSize)
