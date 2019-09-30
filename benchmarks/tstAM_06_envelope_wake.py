5a465588420647c2c18df415d9df5c430fe9be81
massimo <massimo@llrlsi-gw.in2p3.fr>
2019-09-30 11:52:39 +0200

    added ref for AM envelope in vacuum, changed validations for AM envelope

--- benchmarks/tstAM_06_envelope_wake.py
+++ benchmarks/tstAM_06_envelope_wake.py
@@ -1,15 +1,15 @@
 ############################# Laser envelope propagation in vacuum
-dx = 1. 
-dr = 3. 
-dt = 0.8*dx
-nx = 128
-nr = 64
+dx = 0.69 
+dr = 5. 
+dt = 0.57#0.8*dx
+nx = 256
+nr = 60
 Lx = nx * dx
 Lr = nr*dr
-npatch_x=16
-laser_fwhm = 20. 
+npatch_x=32
+laser_fwhm = 70.7 
 center_laser = 2*laser_fwhm # here is the same as focus position of laser but in principle they can differ
-time_start_moving_window = 0.
+time_start_moving_window = Lx-4*laser_fwhm
 
 
 Main(
@@ -18,14 +18,14 @@ Main(
     interpolation_order = 2,
 
     timestep = dt,
-    simulation_time = 300.*dt,
+    simulation_time = 1700.*dt,
 
     cell_length  = [dx, dr],
     grid_length = [ Lx,  Lr],
 
     number_of_AM = 1,
 
-    number_of_patches = [npatch_x,8],
+    number_of_patches = [npatch_x,4],
     clrw = nx/npatch_x,
 
     EM_boundary_conditions = [
@@ -52,14 +52,47 @@ LoadBalancing(
 )
 
 LaserEnvelopeGaussianAM(
-    a0              = 1.,
+    a0              = 0.01,
     focus           = [center_laser, Main.grid_length[1]/2.],
-    waist           = 30.,
+    waist           = 94.26,
     time_envelope   = tgaussian(center=center_laser, fwhm=laser_fwhm),
     envelope_solver = 'explicit',
     Envelope_boundary_conditions = [ ["reflective", "reflective"],
         ["reflective", "reflective"], ],
 )
+
+
+n0 = 0.0017
+Radius_plasma = 300.
+longitudinal_profile = polygonal(xpoints=[3.*laser_fwhm,3.5*laser_fwhm,24.5*laser_fwhm,25.*laser_fwhm],xvalues=[0.,n0,n0,0.])
+def nplasma(x,r):
+    profile_r = 0.
+    if ((r)**2<Radius_plasma**2):
+        profile_r = 1.
+    return profile_r*longitudinal_profile(x,r)
+
+
+
+Species(
+    name = "electron",
+    position_initialization = "regular",
+    momentum_initialization = "cold",
+    particles_per_cell = 4,
+    c_part_max = 1.0,
+    mass = 1.0,
+    charge = -1.0,
+    charge_density = nplasma,
+    mean_velocity = [0.0, 0.0, 0.0],
+    temperature = [0.,0.,0.],
+    pusher = "ponderomotive_boris",
+    ponderomotive_dynamics = "True",
+    time_frozen = 0.0,
+    boundary_conditions = [
+       ["remove", "remove"],
+       ["remove", "remove"],
+    ],
+)
+
 
 Checkpoints(
     dump_step = 0,
@@ -70,28 +103,33 @@ Checkpoints(
 
 DiagFields(
     every = 100,
-    fields = ['Env_A_abs_mode_0','Env_E_abs_mode_0' ]
+    fields = ['Env_A_abs_mode_0','Env_Chi_mode_0','Env_E_abs_mode_0' ]
 )
 
+DiagFields(
+    every = 100,
+    fields = ['El' ]
+)
 
 DiagProbe(
-        every = 20,
+        every = 50,
         origin = [0., 2.*dr, 2.*dr],
         corners = [
             [Main.grid_length[0], 2.*dr, 2.*dr]
         ],
         number = [nx],
-        fields = ['Env_A_abs','Env_E_abs']
+        fields = ['Ex','Ey','Rho','Jx','Env_A_abs','Env_Chi','Env_E_abs']
 )
 
 
 DiagProbe(
-    every = 20,
+    every = 50,
     origin   = [0., -nr*dr,0.],
     corners  = [ [nx*dx,-nr*dr,0.], [0,nr*dr,0.] ],
     number   = [nx, 2*nr],
-    fields = ['Env_A_abs','Env_E_abs']
+    fields = ['Ex','Ey','Rho','Jx','Env_A_abs','Env_Chi','Env_E_abs']
 )
+
 
 
 #DiagScalar(every = 10, vars=['Env_A_absMax','Env_E_absMax'])
