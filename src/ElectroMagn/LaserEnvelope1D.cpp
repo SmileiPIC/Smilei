ca0449e0a169cc84c695b39e44033e1e801bff62
massimo <massimo@poltrnd01.in2p3.fr>
2020-03-18 17:00:15 +0100

    correction to coefficients  of the stencil

--- src/ElectroMagn/LaserEnvelope1D.cpp
+++ src/ElectroMagn/LaserEnvelope1D.cpp
@@ -252,11 +252,11 @@ void LaserEnvelope1D::updateEnvelopeReducedDispersion( ElectroMagn *EMfields )
         ( *A1Dnew )( i ) -= ( *Env_Chi1D )( i )*( *A1D )( i ); // subtract here source term Chi*A from plasma
         // A1Dnew = laplacian - source term
         ( *A1Dnew )( i ) += (1.-2.*delta2)*( ( *A1D )( i-1 )-2.*( *A1D )( i )+( *A1D )( i+1 ) )*one_ov_dx_sq; // x part with optimized stencil
-        ( *A1Dnew )( i ) += delta2*( ( *A1D )( i-2 )-2.*( *A1D )( i-1 )-2.*( *A1D )( i+1 )+( *A1D )( i+2 )   )*one_ov_dx_sq;
+        ( *A1Dnew )( i ) += delta2*( ( *A1D )( i-2 )-2.*( *A1D )( i-1 ) +2.*( *A1D )( i ) -2.*( *A1D )( i+1 )+( *A1D )( i+2 )   )*one_ov_dx_sq;
 
         // A1Dnew = A1Dnew+2ik0*dA/dx, where dA/dx uses the optimized form
         ( *A1Dnew )( i ) += i1_2k0_over_2dx*(1.-2.*delta1)*( ( *A1D )( i+1 )-( *A1D )( i-1 ) );
-        ( *A1Dnew )( i ) += i1_2k0_over_2dx*delta1*( ( *A1D )( i+2 )+( *A1D )( i-2 ) );
+        ( *A1Dnew )( i ) += i1_2k0_over_2dx*delta1*( ( *A1D )( i+2 )-( *A1D )( i-2 ) );
 
         // A1Dnew = A1Dnew*dt^2
         ( *A1Dnew )( i )  = ( *A1Dnew )( i )*dt_sq;
