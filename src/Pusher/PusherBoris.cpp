3871492d62d31d27231ca13ebb10ed1f6322fbbc
Julien <julien.derouillat@cea.fr>
2020-10-14 09:51:42 +0200

    Fix new implementation in debug mode

--- src/Pusher/PusherBoris.cpp
+++ src/Pusher/PusherBoris.cpp
@@ -107,8 +107,12 @@ void PusherBoris::operator()( Particles &particles, SmileiMPI *smpi, int istart,
         
         // Move the particle
 #ifdef  __DEBUG
-        for( int i = 0 ; i<nDim_ ; i++ ) {
-            position_old[i][ipart] = position[i][ipart];
+        position_old[0][ipart] = position_x[ipart];
+        if (nDim_>1) {
+            position_old[1][ipart] = position_y[ipart];
+            if (nDim_>2) {
+                position_old[2][ipart] = position_z[ipart];
+            }
         }
 #endif
         position_x[ipart] += dt*momentum_x[ipart]*invgf[ipart];
