05bae57162e231111c5a4b080d652db634a484e4
Z10Frank <zfrank.massimo@gmail.com>
2021-01-06 15:28:38 +0100

    safe formulation for interpolation with tasks

--- src/Interpolator/Interpolator2D2Order.cpp
+++ src/Interpolator/Interpolator2D2Order.cpp
@@ -19,6 +19,8 @@ Interpolator2D2Order::Interpolator2D2Order( Params &params, Patch *patch ) : Int
 
     dx_inv_ = 1.0/params.cell_length[0];
     dy_inv_ = 1.0/params.cell_length[1];
+
+    tasks_on_projection = params.tasks_on_projection;
     
 }
 
@@ -54,6 +56,55 @@ void Interpolator2D2Order::fields( ElectroMagn *EMfields, Particles &particles, 
     *( BLoc+1*nparts ) = compute( &coeffxd_[1], &coeffyp_[1], By2D, id_, jp_ );
     // Interpolation of Bz^(d,d)
     *( BLoc+2*nparts ) = compute( &coeffxd_[1], &coeffyd_[1], Bz2D, id_, jd_ );
+} // END Interpolator2D2Order
+
+// ---------------------------------------------------------------------------------------------------------------------
+// 2nd Order Interpolation of the fields at a the particle position (3 nodes are used), task-safe
+// ---------------------------------------------------------------------------------------------------------------------
+void Interpolator2D2Order::fieldsForTasks( ElectroMagn *EMfields, Particles &particles, int ipart, int nparts, double *ELoc, double *BLoc, int *iold, double *delta)
+{
+    
+    // Static cast of the electromagnetic fields
+    Field2D *Ex2D = static_cast<Field2D *>( EMfields->Ex_ );
+    Field2D *Ey2D = static_cast<Field2D *>( EMfields->Ey_ );
+    Field2D *Ez2D = static_cast<Field2D *>( EMfields->Ez_ );
+    Field2D *Bx2D = static_cast<Field2D *>( EMfields->Bx_m );
+    Field2D *By2D = static_cast<Field2D *>( EMfields->By_m );
+    Field2D *Bz2D = static_cast<Field2D *>( EMfields->Bz_m );
+    
+    
+    // Normalized particle position
+    double xpn = particles.position( 0, ipart )*dx_inv_;
+    double ypn = particles.position( 1, ipart )*dy_inv_;
+   
+    // Calculate coeffs
+
+    int idx_p[2], idx_d[2];
+    double delta_p[2];
+    double coeffxp[2], coeffyp[2];
+    double coeffxd[2], coeffyd[2];
+
+    coeffs( xpn, ypn, idx_p, idx_d, coeffxp, coeffyp, coeffxd, coeffyd, delta_p );
+
+    // Interpolation of Ex^(d,p)
+    *( ELoc+0*nparts ) = compute( &coeffxd[1], &coeffyp[1], Ex2D, idx_d[0], idx_p[1] );
+    // Interpolation of Ey^(p,d)
+    *( ELoc+1*nparts ) = compute( &coeffxp[1], &coeffyd[1], Ey2D, idx_p[0], idx_d[1] );
+    // Interpolation of Ez^(p,p)
+    *( ELoc+2*nparts ) = compute( &coeffxp[1], &coeffyp[1], Ez2D, idx_p[0], idx_p[1] );
+    // Interpolation of Bx^(p,d)
+    *( BLoc+0*nparts ) = compute( &coeffxp[1], &coeffyd[1], Bx2D, idx_p[0], idx_d[1] );
+    // Interpolation of By^(d,p)
+    *( BLoc+1*nparts ) = compute( &coeffxd[1], &coeffyp[1], By2D, idx_d[0], idx_p[1] );
+    // Interpolation of Bz^(d,d)
+    *( BLoc+2*nparts ) = compute( &coeffxd[1], &coeffyd[1], Bz2D, idx_d[0], idx_d[1] );
+
+    //Buffering of iol and delta
+    *( iold+0*nparts)  = idx_p[0];
+    *( iold+1*nparts)  = idx_p[1];
+    *( delta+0*nparts) = delta_p[0];
+    *( delta+1*nparts) = delta_p[1];
+   
 } // END Interpolator2D2Order
 
 void Interpolator2D2Order::fieldsAndCurrents( ElectroMagn *EMfields, Particles &particles, SmileiMPI *smpi, int *istart, int *iend, int ithread, LocalFields *JLoc, double *RhoLoc )
@@ -133,14 +184,22 @@ void Interpolator2D2Order::fieldsWrapper( ElectroMagn *EMfields, Particles &part
     
     //Loop on bin particles
     int nparts( particles.size() );
-    for( int ipart=*istart ; ipart<*iend; ipart++ ) {
-        //Interpolation on current particle
-        fields( EMfields, particles, ipart, nparts, &( *Epart )[ipart], &( *Bpart )[ipart] );
-        //Buffering of iol and delta
-        ( *iold )[ipart+0*nparts]  = ip_;
-        ( *iold )[ipart+1*nparts]  = jp_;
-        ( *delta )[ipart+0*nparts] = deltax;
-        ( *delta )[ipart+1*nparts] = deltay;
+
+    if (!tasks_on_projection){
+        for( int ipart=*istart ; ipart<*iend; ipart++ ) {
+            //Interpolation on current particle
+            fields( EMfields, particles, ipart, nparts, &( *Epart )[ipart], &( *Bpart )[ipart] );
+            //Buffering of iol and delta
+            ( *iold )[ipart+0*nparts]  = ip_;
+            ( *iold )[ipart+1*nparts]  = jp_;
+            ( *delta )[ipart+0*nparts] = deltax;
+            ( *delta )[ipart+1*nparts] = deltay;
+        }
+    } else {
+        for( int ipart=*istart ; ipart<*iend; ipart++ ) {
+            //Interpolation on current particle with locally defined variables to avoid data races between threads
+            fieldsForTasks( EMfields, particles, ipart, nparts, &( *Epart )[ipart], &( *Bpart )[ipart], &( *iold )[ipart] , &( *delta )[ipart] );
+        }
     }
     
 }
--- src/Interpolator/Interpolator2D2Order.h
+++ src/Interpolator/Interpolator2D2Order.h
@@ -17,6 +17,7 @@ public:
     ~Interpolator2D2Order() override final {};
     
     inline void fields( ElectroMagn *EMfields, Particles &particles, int ipart, int nparts, double *ELoc, double *BLoc );
+    inline void fieldsForTasks( ElectroMagn *EMfields, Particles &particles, int ipart, int nparts, double *ELoc, double *BLoc, int *iold, double *delta );
     void fieldsAndCurrents( ElectroMagn *EMfields, Particles &particles, SmileiMPI *smpi, int *istart, int *iend, int ithread, LocalFields *JLoc, double *RhoLoc ) override final ;
     void fieldsWrapper( ElectroMagn *EMfields, Particles &particles, SmileiMPI *smpi, int *istart, int *iend, int ithread, int ipart_ref = 0 ) override final ;
     void fieldsSelection( ElectroMagn *EMfields, Particles &particles, double *buffer, int offset, std::vector<unsigned int> *selection ) override final;
@@ -38,6 +39,8 @@ public:
     void timeCenteredEnvelope( ElectroMagn *EMfields, Particles &particles, SmileiMPI *smpi, int *istart, int *iend, int ithread, int ipart_ref = 0 ) override final;
     void envelopeAndSusceptibility( ElectroMagn *EMfields, Particles &particles, int ipart, double *Env_A_abs_Loc, double *Env_Chi_Loc, double *Env_E_abs_Loc, double *Env_Ex_abs_Loc ) override final;
     void envelopeFieldForIonization( ElectroMagn *EMfields, Particles &particles, SmileiMPI *smpi, int *istart, int *iend, int ithread, int ipart_ref = 0 ) override final;    
+
+    bool tasks_on_projection;
 
 private:
     inline void coeffs( double xpn, double ypn )
@@ -81,6 +84,52 @@ private:
         id_ = id_ - i_domain_begin;
         jp_ = jp_ - j_domain_begin;
         jd_ = jd_ - j_domain_begin;
+    }
+
+    inline void coeffs( double xpn, double ypn, int* idx_p, int* idx_d,
+                        double *coeffxp, double *coeffyp,
+                        double *coeffxd, double *coeffyd, double* delta_p )
+    {
+        // Indexes of the central nodes
+        idx_p[0] = round( xpn );
+        idx_d[0] = round( xpn+0.5 );
+        idx_p[1] = round( ypn );
+        idx_d[1] = round( ypn+0.5 );
+
+        // Declaration and calculation of the coefficient for interpolation
+        double delta, delta2;
+
+        delta   = xpn - ( double )idx_d[0] + 0.5;
+        delta2  = delta*delta;
+        coeffxd[0] = 0.5 * ( delta2-delta+0.25 );
+        coeffxd[1] = 0.75 - delta2;
+        coeffxd[2] = 0.5 * ( delta2+delta+0.25 );
+
+        delta_p[0]   = xpn - ( double )idx_p[0];
+        delta2  = delta_p[0]*delta_p[0];
+        coeffxp[0] = 0.5 * ( delta2-delta_p[0]+0.25 );
+        coeffxp[1] = 0.75 - delta2;
+        coeffxp[2] = 0.5 * ( delta2+delta_p[0]+0.25 );
+
+        delta   = ypn - ( double )idx_d[1] + 0.5;
+        delta2  = delta*delta;
+        coeffyd[0] = 0.5 * ( delta2-delta+0.25 );
+        coeffyd[1] = 0.75 - delta2;
+        coeffyd[2] = 0.5 * ( delta2+delta+0.25 );
+
+        delta_p[1]   = ypn - ( double )idx_p[1];
+        delta2  = delta_p[1]*delta_p[1];
+        coeffyp[0] = 0.5 * ( delta2-delta_p[1]+0.25 );
+        coeffyp[1] = 0.75 - delta2;
+        coeffyp[2] = 0.5 * ( delta2+delta_p[1]+0.25 );
+
+        //!\todo CHECK if this is correct for both primal & dual grids !!!
+        // First index for summation
+        idx_p[0] = idx_p[0] - i_domain_begin;
+        idx_d[0] = idx_d[0] - i_domain_begin;
+        idx_p[1] = idx_p[1] - j_domain_begin;
+        idx_d[1] = idx_d[1] - j_domain_begin;
+        
     }
     
     // Last prim index computed
