9b20d863a69adb741032b740cfe4185cb5a8ac46
massimo <massimo@poltrnd01.in2p3.fr>
2021-01-07 19:07:11 +0100

    add projection on buffers

--- src/Species/Species_taskomp.cpp
+++ src/Species/Species_taskomp.cpp
@@ -264,11 +264,24 @@ void Species_taskomp::dynamicsWithTasks( double time_dual, unsigned int ispec,
                 timer = MPI_Wtime();
 #endif
 
-                // Project currents if not a Test species and charges as well if a diag is needed.
-                // Do not project if a photon
-                if( ( !particles->is_test ) && ( mass_ > 0 ) ) {
-                    Proj->currentsAndDensityWrapper( EMfields, *particles, smpi, particles->first_index[ibin], particles->last_index[ibin], ithread, diag_flag, params.is_spectral, ispec );
-                }
+                // Reset densities sub-buffers
+             for (int i = 0; i < size_proj_buffer_Jx; i++)  b_Jx [ibin][i]  = 0.0;
+             for (int i = 0; i < size_proj_buffer_Jy; i++)  b_Jy [ibin][i]  = 0.0;
+             for (int i = 0; i < size_proj_buffer_Jz; i++)  b_Jz [ibin][i]  = 0.0;
+             for (int i = 0; i < size_proj_buffer_rho; i++) b_rho[ibin][i] = 0.0;
+ 
+             // Project currents if not a Test species and charges as well if a diag is needed.
+             // Do not project if a photon
+             if( ( !particles->is_test ) && ( mass_ > 0 ) ) {
+                 if (params.geometry == "2Dcartesian"){
+                     Projector2D2Order *Proj2D = static_cast<Projector2D2Order *>(Proj);
+                     Proj2D->currentsAndDensityWrapperOnBuffers( b_Jx[ibin], b_Jy[ibin], b_Jz[ibin], b_rho[ibin], ibin*clrw, *particles, smpi, particles->first_index[ibin], particles->last_index[ibin], ithread, diag_flag, params.is_spectral, ispec );
+                 }
+                 // } else if (params.geometry == "3Dcartesian"){
+                 //     Projector3D2Order *Proj3D = static_cast<Projector3D2Order *>(Proj);
+                 //     Proj3D->currentsAndDensityWrapperOnBuffers( b_Jx[ibin], b_Jy[ibin], b_Jz[ibin], b_rho[ibin], ibin*clrw, *particles, smpi, particles->first_index[ibin], particles->last_index[ibin], ithread, diag_flag, params.is_spectral, ispec );
+                 // } else {ERROR("Task strategy not yet implemented in 1Dcartesian or AMcylindrical geometries");}
+             }
 
 #ifdef  __DETAILED_TIMERS
                 patch->patch_timers[2] += MPI_Wtime() - timer;
