0c76ac01d4f261fdd906b575674067f526601560
Francesco Massimo <zfrank.massimo@gmail.com>
2021-05-12 19:56:47 +0200

    avoid projecting densities twice when envelope is present

--- src/Patch/VectorPatch.cpp
+++ src/Patch/VectorPatch.cpp
@@ -470,25 +470,28 @@ void VectorPatch::dynamics( Params &params,
 #endif
             
             for( unsigned int ispec=0 ; ispec<Nspecies ; ispec++ ) {
-                // DO NOT parallelize this species loop unless race condition prevention is used!
-                Species *spec_task = species( ipatch, ispec );
-                std::vector<unsigned int> b_dim = spec_task->b_dim;
-                for( unsigned int ibin = 0 ; ibin < spec_task->Nbins  ; ibin++ ) {
-                    if (params.geometry != "AMcylindrical"){
-                        double *b_Jx             = spec_task->b_Jx[ibin];
-                        double *b_Jy             = spec_task->b_Jy[ibin];
-                        double *b_Jz             = spec_task->b_Jz[ibin];
-                        double *b_rho            = spec_task->b_rho[ibin];
-                        (( *this )( ipatch )->EMfields)->copyInLocalDensities(ispec, ibin*clrw, b_Jx, b_Jy, b_Jz, b_rho, b_dim, diag_flag);
-                    } else { // AM geometry
-                        complex<double> *b_Jl    = spec_task->b_Jl[ibin];
-                        complex<double> *b_Jr    = spec_task->b_Jr[ibin];
-                        complex<double> *b_Jt    = spec_task->b_Jt[ibin];
-                        complex<double> *b_rhoAM = spec_task->b_rhoAM[ibin];
-                        ElectroMagnAM *emAM = static_cast<ElectroMagnAM *>( ( *this )( ipatch )->EMfields );
-                        emAM->copyInLocalAMDensities(ispec, ibin*clrw, b_Jl, b_Jr, b_Jt, b_rhoAM, b_dim, diag_flag);
-                    }
-                } // ibin
+                if (!(species( ipatch, ispec )->ponderomotive_dynamics)){  
+                    // Reduction with envelope must be performed only after VectorPatch::runEnvelopeModule, which is after VectorPatch::dynamics
+                    // DO NOT parallelize this species loop unless race condition prevention is used!
+                    Species *spec_task = species( ipatch, ispec );
+                    std::vector<unsigned int> b_dim = spec_task->b_dim;
+                    for( unsigned int ibin = 0 ; ibin < spec_task->Nbins  ; ibin++ ) {
+                        if (params.geometry != "AMcylindrical"){
+                            double *b_Jx             = spec_task->b_Jx[ibin];
+                            double *b_Jy             = spec_task->b_Jy[ibin];
+                            double *b_Jz             = spec_task->b_Jz[ibin];
+                            double *b_rho            = spec_task->b_rho[ibin];
+                            (( *this )( ipatch )->EMfields)->copyInLocalDensities(ispec, ibin*clrw, b_Jx, b_Jy, b_Jz, b_rho, b_dim, diag_flag);
+                        } else { // AM geometry
+                            complex<double> *b_Jl    = spec_task->b_Jl[ibin];
+                            complex<double> *b_Jr    = spec_task->b_Jr[ibin];
+                            complex<double> *b_Jt    = spec_task->b_Jt[ibin];
+                            complex<double> *b_rhoAM = spec_task->b_rhoAM[ibin];
+                            ElectroMagnAM *emAM = static_cast<ElectroMagnAM *>( ( *this )( ipatch )->EMfields );
+                            emAM->copyInLocalAMDensities(ispec, ibin*clrw, b_Jl, b_Jr, b_Jt, b_rhoAM, b_dim, diag_flag);
+                        }
+                    } // ibin
+                }
             } // end species loop
 
 #ifdef  __DETAILED_TIMERS
@@ -4523,25 +4526,28 @@ void VectorPatch::ponderomotiveUpdatePositionAndCurrents( Params &params,
 #endif
             
             for( unsigned int ispec=0 ; ispec<Nspecies ; ispec++ ) {
-                // DO NOT parallelize this species loop unless race condition prevention is used!
-                Species *spec_task = species( ipatch, ispec );
-                std::vector<unsigned int> b_dim = spec_task->b_dim;
-                for( unsigned int ibin = 0 ; ibin < spec_task->Nbins  ; ibin++ ) {
-                    if (params.geometry != "AMcylindrical"){
-                        double *b_Jx             = spec_task->b_Jx[ibin];
-                        double *b_Jy             = spec_task->b_Jy[ibin];
-                        double *b_Jz             = spec_task->b_Jz[ibin];
-                        double *b_rho            = spec_task->b_rho[ibin];
-                        (( *this )( ipatch )->EMfields)->copyInLocalDensities(ispec, ibin*clrw, b_Jx, b_Jy, b_Jz, b_rho, b_dim, diag_flag);
-                    } else { // AM geometry
-                        complex<double> *b_Jl    = spec_task->b_Jl[ibin];
-                        complex<double> *b_Jr    = spec_task->b_Jr[ibin];
-                        complex<double> *b_Jt    = spec_task->b_Jt[ibin];
-                        complex<double> *b_rhoAM = spec_task->b_rhoAM[ibin];
-                        ElectroMagnAM *emAM = static_cast<ElectroMagnAM *>( ( *this )( ipatch )->EMfields );
-                        emAM->copyInLocalAMDensities(ispec, ibin*clrw, b_Jl, b_Jr, b_Jt, b_rhoAM, b_dim, diag_flag);
-                    }
-                } // ibin
+                if ((species( ipatch, ispec )->ponderomotive_dynamics)){  
+                    // Reduction with envelope must be performed only after VectorPatch::runEnvelopeModule, which is after VectorPatch::dynamics
+                    // DO NOT parallelize this species loop unless race condition prevention is used!
+                    Species *spec_task = species( ipatch, ispec );
+                    std::vector<unsigned int> b_dim = spec_task->b_dim;
+                    for( unsigned int ibin = 0 ; ibin < spec_task->Nbins  ; ibin++ ) {
+                        if (params.geometry != "AMcylindrical"){
+                            double *b_Jx             = spec_task->b_Jx[ibin];
+                            double *b_Jy             = spec_task->b_Jy[ibin];
+                            double *b_Jz             = spec_task->b_Jz[ibin];
+                            double *b_rho            = spec_task->b_rho[ibin];
+                            (( *this )( ipatch )->EMfields)->copyInLocalDensities(ispec, ibin*clrw, b_Jx, b_Jy, b_Jz, b_rho, b_dim, diag_flag);
+                        } else { // AM geometry
+                            complex<double> *b_Jl    = spec_task->b_Jl[ibin];
+                            complex<double> *b_Jr    = spec_task->b_Jr[ibin];
+                            complex<double> *b_Jt    = spec_task->b_Jt[ibin];
+                            complex<double> *b_rhoAM = spec_task->b_rhoAM[ibin];
+                            ElectroMagnAM *emAM = static_cast<ElectroMagnAM *>( ( *this )( ipatch )->EMfields );
+                            emAM->copyInLocalAMDensities(ispec, ibin*clrw, b_Jl, b_Jr, b_Jt, b_rhoAM, b_dim, diag_flag);
+                        }
+                    } // ibin
+                }
             } // end species loop
 
 #ifdef  __DETAILED_TIMERS
