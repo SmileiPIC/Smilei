a346ac6246996b0f2de194313b21501ef32fd609
Mathieu Lobet <mathieu.lobet@cea.fr>
2021-09-20 14:53:22 +0200

    explicit vecto for cell keys computation

--- src/Species/SpeciesV.cpp
+++ src/Species/SpeciesV.cpp
@@ -363,6 +363,7 @@ void SpeciesV::dynamics( double time_dual, unsigned int ispec,
 
             } else if (nDim_field == 3) {
 
+                #pragma omp simd
                 for( iPart=particles->first_index[ipack*packsize_] ; iPart<particles->last_index[ipack*packsize_+packsize_-1]; iPart++ ) {
                     if ( particles->cell_keys[iPart] != -1 ) {
                         //Compute cell_keys of remaining particles
@@ -372,7 +373,11 @@ void SpeciesV::dynamics( double time_dual, unsigned int ispec,
                         particles->cell_keys[iPart] += round( (particles->position(1, iPart) - min_loc_vec[1]) * dx_inv_[1] );
                         particles->cell_keys[iPart] *= length[2];
                         particles->cell_keys[iPart] += round( (particles->position(2, iPart) - min_loc_vec[2]) * dx_inv_[2] );
+                    }
+                }
 
+                for( iPart=particles->first_index[ipack*packsize_] ; iPart<particles->last_index[ipack*packsize_+packsize_-1]; iPart++ ) {
+                    if ( particles->cell_keys[iPart] != -1 ) {
                         count[particles->cell_keys[iPart]] ++;
                     }
                 }
