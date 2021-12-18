e0269152eeb79d02c2075d3c965441e1b916afaa
Yingchao Lu <yingchao.lu@gmail.com>
2021-12-17 21:54:23 -0500

    added WT interpolation

---
+++ src/Interpolator/Interpolator1DWT2Order.cpp
@@ -0,0 +1,364 @@
+#include "Interpolator1DWT2Order.h"
+
+#include <cmath>
+#include <iostream>
+
+#include "ElectroMagn.h"
+#include "Field1D.h"
+#include "Particles.h"
+#include "LaserEnvelope.h"
+
+
+using namespace std;
+
+Interpolator1DWT2Order::Interpolator1DWT2Order( Params &params, Patch *patch ) : Interpolator1D( params, patch )
+{
+    dx_inv_ = 1.0/params.cell_length[0];
+    dt_ov_dx = params.timestep/params.cell_length[0];
+    dx_ov_8dt = params.cell_length[0]/params.timestep/8.0;
+}
+
+// ---------------------------------------------------------------------------------------------------------------------
+// 2nd Order WT Interpolation of the fields at a the particle position (3 nodes are used)
+// ---------------------------------------------------------------------------------------------------------------------
+void Interpolator1DWT2Order::fields( ElectroMagn *EMfields, Particles &particles, int ipart, int nparts, double *ELoc, double *BLoc )
+{
+    // Static cast of the electromagnetic fields
+    Field1D *Ex1D     = static_cast<Field1D *>( EMfields->Ex_ );
+    Field1D *Ey1D     = static_cast<Field1D *>( EMfields->Ey_ );
+    Field1D *Ez1D     = static_cast<Field1D *>( EMfields->Ez_ );
+    Field1D *Bx1D_m   = static_cast<Field1D *>( EMfields->Bx_m );
+    Field1D *By1D_m   = static_cast<Field1D *>( EMfields->By_m );
+    Field1D *Bz1D_m   = static_cast<Field1D *>( EMfields->Bz_m );
+
+    // Particle position (in units of the spatial-step)
+    double xjn = particles.position( 0, ipart )*dx_inv_;
+    // Calculate coeffs
+    coeffs( xjn );
+
+    // Interpolate the fields from the Dual grid : Ex, By, Bz
+    *( ELoc+0*nparts ) = compute( coeffd_, Ex1D,   id_ );
+    *( BLoc+1*nparts ) = compute( coeffd_, By1D_m, id_ );
+    *( BLoc+2*nparts ) = compute( coeffd_, Bz1D_m, id_ );
+
+    // Interpolate the fields from the Primal grid (WT) : Ey, Ez, Bx
+    *( ELoc+1*nparts ) = compute( coeffpt_, Ey1D,   ip_ );
+    *( ELoc+2*nparts ) = compute( coeffpt_, Ez1D,   ip_ );
+    *( BLoc+0*nparts ) = compute( coeffpt_, Bx1D_m, ip_ );
+
+}//END Interpolator1DWT2Order
+
+void Interpolator1DWT2Order::fieldsAndCurrents( ElectroMagn *EMfields, Particles &particles, SmileiMPI *smpi, int *istart, int *iend, int ithread, LocalFields *JLoc, double *RhoLoc )
+{
+    int ipart = *istart;
+
+    double *ELoc = &( smpi->dynamics_Epart[ithread][ipart] );
+    double *BLoc = &( smpi->dynamics_Bpart[ithread][ipart] );
+
+    // Static cast of the electromagnetic fields
+    Field1D *Ex1D     = static_cast<Field1D *>( EMfields->Ex_ );
+    Field1D *Ey1D     = static_cast<Field1D *>( EMfields->Ey_ );
+    Field1D *Ez1D     = static_cast<Field1D *>( EMfields->Ez_ );
+    Field1D *Bx1D_m   = static_cast<Field1D *>( EMfields->Bx_m );
+    Field1D *By1D_m   = static_cast<Field1D *>( EMfields->By_m );
+    Field1D *Bz1D_m   = static_cast<Field1D *>( EMfields->Bz_m );
+    Field1D *Jx1D     = static_cast<Field1D *>( EMfields->Jx_ );
+    Field1D *Jy1D     = static_cast<Field1D *>( EMfields->Jy_ );
+    Field1D *Jz1D     = static_cast<Field1D *>( EMfields->Jz_ );
+    Field1D *Rho1D    = static_cast<Field1D *>( EMfields->rho_ );
+
+    // Particle position (in units of the spatial-step)
+    double xjn = particles.position( 0, ipart )*dx_inv_;
+    // Calculate coeffs
+    coeffs( xjn );
+
+    int nparts( particles.size() );
+
+    // Interpolate the fields from the Dual grid : Ex, By, Bz
+    *( ELoc+0*nparts ) = compute( coeffd_, Ex1D,   id_ );
+    *( BLoc+1*nparts ) = compute( coeffd_, By1D_m, id_ );
+    *( BLoc+2*nparts ) = compute( coeffd_, Bz1D_m, id_ );
+
+    // Interpolate the fields from the Primal grid : Ey, Ez, Bx
+    *( ELoc+1*nparts ) = compute( coeffpt_, Ey1D,   ip_ );
+    *( ELoc+2*nparts ) = compute( coeffpt_, Ez1D,   ip_ );
+    *( BLoc+0*nparts ) = compute( coeffpt_, Bx1D_m, ip_ );
+
+    // Interpolate the fields from the Primal grid : Jy, Jz, Rho
+    JLoc->y = compute( coeffp_, Jy1D,  ip_ );
+    JLoc->z = compute( coeffp_, Jz1D,  ip_ );
+    ( *RhoLoc ) = compute( coeffp_, Rho1D, ip_ );
+
+    // Interpolate the fields from the Dual grid : Jx
+    JLoc->x = compute( coeffd_, Jx1D,  id_ );
+
+}
+
+// Interpolator on another field than the basic ones
+void Interpolator1DWT2Order::oneField( Field **field, Particles &particles, int *istart, int *iend, double *FieldLoc, double *l1, double *l2, double *l3 )
+{
+    Field1D *F = static_cast<Field1D *>( *field );
+    double *coeff = F->isDual( 0 ) ? coeffd_ : coeffpt_;
+    int *i = F->isDual( 0 ) ? &id_ : &ip_;
+
+    for( int ipart=*istart ; ipart<*iend; ipart++ ) {
+        double xjn = particles.position( 0, ipart )*dx_inv_;
+        coeffs( xjn );
+        FieldLoc[ipart] = compute( coeff, F, *i );
+    }
+}
+
+void Interpolator1DWT2Order::fieldsWrapper( ElectroMagn *EMfields, Particles &particles, SmileiMPI *smpi, int *istart, int *iend, int ithread, int ipart_ref )
+{
+    std::vector<double> *Epart = &( smpi->dynamics_Epart[ithread] );
+    std::vector<double> *Bpart = &( smpi->dynamics_Bpart[ithread] );
+    std::vector<int> *iold = &( smpi->dynamics_iold[ithread] );
+    std::vector<double> *delta = &( smpi->dynamics_deltaold[ithread] );
+
+    //Loop on bin particles
+    int npart_tot = particles.size();
+    for( int ipart=*istart ; ipart<*iend; ipart++ ) {
+        //Interpolation on current particle
+        fields( EMfields, particles, ipart, npart_tot, &( *Epart )[ipart], &( *Bpart )[ipart] );
+        //Buffering of iol and delta
+        ( *iold )[ipart] = ip_;
+        ( *delta )[ipart] = xjmxi;
+    }
+
+}
+
+// Interpolator specific to tracked particles. A selection of particles may be provided
+void Interpolator1DWT2Order::fieldsSelection( ElectroMagn *EMfields, Particles &particles, double *buffer, int offset, vector<unsigned int> *selection )
+{
+    if( selection ) {
+
+        int nsel_tot = selection->size();
+        for( int isel=0 ; isel<nsel_tot; isel++ ) {
+            fields( EMfields, particles, ( *selection )[isel], offset, buffer+isel, buffer+isel+3*offset );
+        }
+
+    } else {
+
+        int npart_tot = particles.size();
+        for( int ipart=0 ; ipart<npart_tot; ipart++ ) {
+            fields( EMfields, particles, ipart, offset, buffer+ipart, buffer+ipart+3*offset );
+        }
+
+    }
+}
+
+void Interpolator1DWT2Order::fieldsAndEnvelope( ElectroMagn *EMfields, Particles &particles, SmileiMPI *smpi, int *istart, int *iend, int ithread, int ipart_ref )
+{
+    // Static cast of the envelope fields
+    Field1D *Phi1D = static_cast<Field1D *>( EMfields->envelope->Phi_ );
+    Field1D *GradPhix1D = static_cast<Field1D *>( EMfields->envelope->GradPhix_ );
+    Field1D *GradPhiy1D = static_cast<Field1D *>( EMfields->envelope->GradPhiy_ );
+    Field1D *GradPhiz1D = static_cast<Field1D *>( EMfields->envelope->GradPhiz_ );
+
+    std::vector<double> *Epart = &( smpi->dynamics_Epart[ithread] );
+    std::vector<double> *Bpart = &( smpi->dynamics_Bpart[ithread] );
+    std::vector<double> *PHIpart        = &( smpi->dynamics_PHIpart[ithread] );
+    std::vector<double> *GradPHIpart    = &( smpi->dynamics_GradPHIpart[ithread] );
+
+    std::vector<int>    *iold  = &( smpi->dynamics_iold[ithread] );
+    std::vector<double> *delta = &( smpi->dynamics_deltaold[ithread] );
+
+    //Loop on bin particles
+    int nparts( particles.size() );
+    for( int ipart=*istart ; ipart<*iend; ipart++ ) {
+
+        fields( EMfields, particles, ipart, nparts, &( *Epart )[ipart], &( *Bpart )[ipart] );
+
+
+        // -------------------------
+        // Interpolation of Phi^(p,p)
+        // -------------------------
+        ( *PHIpart )[ipart] = compute( &coeffp_[1], Phi1D, ip_ );
+
+        // -------------------------
+        // Interpolation of GradPhix^(p,p)
+        // -------------------------
+        ( *GradPHIpart )[ipart+0*nparts] = compute( &coeffp_[1], GradPhix1D, ip_ );
+
+        // -------------------------
+        // Interpolation of GradPhiy^(p,p)
+        // -------------------------
+        ( *GradPHIpart )[ipart+1*nparts] = compute( &coeffp_[1], GradPhiy1D, ip_ );
+
+        // -------------------------
+        // Interpolation of GradPhiz^(p,p)
+        // -------------------------
+        ( *GradPHIpart )[ipart+2*nparts] = compute( &coeffp_[1], GradPhiz1D, ip_ );
+
+
+        //Buffering of iold and delta
+        ( *iold )[ipart+0*nparts]  = ip_;
+        ( *delta )[ipart+0*nparts] = deltax;
+
+    }
+
+
+} // END Interpolator1DWT2Order
+
+
+void Interpolator1DWT2Order::timeCenteredEnvelope( ElectroMagn *EMfields, Particles &particles, SmileiMPI *smpi, int *istart, int *iend, int ithread, int ipart_ref )
+{
+    // Static cast of the envelope fields
+    Field1D *Phi_m1D = static_cast<Field1D *>( EMfields->envelope->Phi_m );
+    Field1D *GradPhix_m1D = static_cast<Field1D *>( EMfields->envelope->GradPhix_m );
+    Field1D *GradPhiy_m1D = static_cast<Field1D *>( EMfields->envelope->GradPhiy_m );
+    Field1D *GradPhiz_m1D = static_cast<Field1D *>( EMfields->envelope->GradPhiz_m );
+
+    std::vector<double> *PHI_mpart     = &( smpi->dynamics_PHI_mpart[ithread] );
+    std::vector<double> *GradPHI_mpart = &( smpi->dynamics_GradPHI_mpart[ithread] );
+
+    std::vector<int>    *iold  = &( smpi->dynamics_iold[ithread] );
+    std::vector<double> *delta = &( smpi->dynamics_deltaold[ithread] );
+
+    //Loop on bin particles
+    int nparts( particles.size() );
+    for( int ipart=*istart ; ipart<*iend; ipart++ ) {
+
+        // Normalized particle position
+        double xpn = particles.position( 0, ipart )*dx_inv_;
+
+        // Indexes of the central nodes
+        ip_ = round( xpn );
+
+        // Declaration and calculation of the coefficient for interpolation
+        double deltax, delta2;
+
+
+        deltax   = xpn - ( double )ip_;
+        delta2  = deltax*deltax;
+        coeffp_[0] = 0.5 * ( delta2-deltax+0.25 );
+        coeffp_[1] = 0.75 - delta2;
+        coeffp_[2] = 0.5 * ( delta2+deltax+0.25 );
+
+
+
+        //!\todo CHECK if this is correct for both primal & dual grids !!!
+        // First index for summation
+        ip_ = ip_ - index_domain_begin;
+
+        // -------------------------
+        // Interpolation of Phiold^(p)
+        // -------------------------
+        ( *PHI_mpart )[ipart] = compute( &coeffp_[1], Phi_m1D, ip_ );
+
+        // -------------------------
+        // Interpolation of GradPhix_m^(p)
+        // -------------------------
+        ( *GradPHI_mpart )[ipart+0*nparts] = compute( &coeffp_[1], GradPhix_m1D, ip_ );
+
+        // -------------------------
+        // Interpolation of GradPhiyold^(p)
+        // -------------------------
+        ( *GradPHI_mpart )[ipart+1*nparts] = compute( &coeffp_[1], GradPhiy_m1D, ip_ );
+
+        // -------------------------
+        // Interpolation of GradPhizold^(p)
+        // -------------------------
+        ( *GradPHI_mpart )[ipart+2*nparts] = compute( &coeffp_[1], GradPhiz_m1D, ip_ );
+
+        //Buffering of iold and delta
+        ( *iold )[ipart+0*nparts]  = ip_;
+        ( *delta )[ipart+0*nparts] = deltax;
+
+
+    }
+
+} // END Interpolator1DWT2Order
+
+
+void Interpolator1DWT2Order::envelopeAndSusceptibility( ElectroMagn *EMfields, Particles &particles, int ipart, double *Env_A_abs_Loc, double *Env_Chi_Loc, double *Env_E_abs_Loc, double *Env_Ex_abs_Loc )
+{
+    // Static cast of the electromagnetic fields
+    Field1D *Env_A_abs_1D  = static_cast<Field1D *>( EMfields->Env_A_abs_ );
+    Field1D *Env_Chi_1D    = static_cast<Field1D *>( EMfields->Env_Chi_ );
+    Field1D *Env_E_abs_1D  = static_cast<Field1D *>( EMfields->Env_E_abs_ );
+    Field1D *Env_Ex_abs_1D = static_cast<Field1D *>( EMfields->Env_Ex_abs_ );
+
+    // Normalized particle position
+    double xpn = particles.position( 0, ipart )*dx_inv_;
+
+    // Indexes of the central nodes
+    ip_ = round( xpn );
+
+    // Declaration and calculation of the coefficient for interpolation
+    double deltax, delta2;
+
+
+    deltax   = xpn - ( double )ip_;
+    delta2  = deltax*deltax;
+    coeffp_[0] = 0.5 * ( delta2-deltax+0.25 );
+    coeffp_[1] = 0.75 - delta2;
+    coeffp_[2] = 0.5 * ( delta2+deltax+0.25 );
+
+
+    //!\todo CHECK if this is correct for both primal & dual grids !!!
+    // First index for summation
+    ip_ = ip_ - index_domain_begin;
+
+    // -------------------------
+    // Interpolation of Env_A_abs_^(p)
+    // -------------------------
+    *( Env_A_abs_Loc ) = compute( &coeffp_[1], Env_A_abs_1D, ip_ );
+
+    // -------------------------
+    // Interpolation of Env_Chi_^(p)
+    // -------------------------
+    *( Env_Chi_Loc ) = compute( &coeffp_[1], Env_Chi_1D, ip_ );
+
+    // -------------------------
+    // Interpolation of Env_E_abs_^(p)
+    // -------------------------
+    *( Env_E_abs_Loc ) = compute( &coeffp_[1], Env_E_abs_1D, ip_ );
+
+    // -------------------------
+    // Interpolation of Env_Ex_abs_^(p)
+    // -------------------------
+    *( Env_Ex_abs_Loc ) = compute( &coeffp_[1], Env_Ex_abs_1D, ip_ );
+
+} // END Interpolator1DWT2Order
+
+
+void Interpolator1DWT2Order::envelopeFieldForIonization( ElectroMagn *EMfields, Particles &particles, SmileiMPI *smpi, int *istart, int *iend, int ithread, int ipart_ref )
+{
+    // Static cast of the envelope fields
+    Field1D *Env_Eabs = static_cast<Field1D *>( EMfields->Env_E_abs_ );
+
+    std::vector<double> *Env_Eabs_part = &( smpi->dynamics_EnvEabs_part[ithread] );
+
+    //Loop on bin particles
+    for( int ipart=*istart ; ipart<*iend; ipart++ ) {
+
+        // Normalized particle position
+        double xpn = particles.position( 0, ipart )*dx_inv_;
+
+        // Indexes of the central nodes
+        ip_ = round( xpn );
+
+        // Declaration and calculation of the coefficient for interpolation
+        double deltax, delta2;
+
+        deltax   = xpn - ( double )ip_;
+        delta2  = deltax*deltax;
+        coeffp_[0] = 0.5 * ( delta2-deltax+0.25 );
+        coeffp_[1] = 0.75 - delta2;
+        coeffp_[2] = 0.5 * ( delta2+deltax+0.25 );
+
+
+        //!\todo CHECK if this is correct for both primal & dual grids !!!
+        // First index for summation
+        ip_ = ip_ - index_domain_begin;
+
+        // ---------------------------------
+        // Interpolation of Env_E_abs^(p)
+        // ---------------------------------
+        ( *Env_Eabs_part )[ipart] = compute( &coeffp_[1], Env_Eabs, ip_ );
+
+    }
+
+} // END Interpolator1DWT2Order
+
--- src/Interpolator/InterpolatorFactory.h
+++ src/Interpolator/InterpolatorFactory.h
@@ -11,6 +11,12 @@
 #include "Interpolator3D4Order.h"
 #include "InterpolatorAM1Order.h"
 #include "InterpolatorAM2Order.h"
+#include "Interpolator1DWT2Order.h"
+#include "Interpolator1DWT4Order.h"
+#include "Interpolator2DWT2Order.h"
+#include "Interpolator2DWT4Order.h"
+#include "Interpolator3DWT2Order.h"
+#include "Interpolator3DWT4Order.h"
 
 #ifdef _VECTO
 #include "Interpolator1D2OrderV.h"
@@ -18,6 +24,11 @@
 #include "Interpolator2D4OrderV.h"
 #include "Interpolator3D2OrderV.h"
 #include "Interpolator3D4OrderV.h"
+#include "Interpolator1DWT2OrderV.h"
+#include "Interpolator2DWT2OrderV.h"
+#include "Interpolator2DWT4OrderV.h"
+#include "Interpolator3DWT2OrderV.h"
+#include "Interpolator3DWT4OrderV.h"
 #endif
 
 #include "Params.h"
@@ -30,41 +41,84 @@ class InterpolatorFactory
 public:
     static Interpolator *create( Params &params, Patch *patch, bool vectorization )
     {
+        bool wt_used = false;
         Interpolator *Interp = NULL;
         // ---------------
         // 1Dcartesian simulation
         // ---------------
         if( ( params.geometry == "1Dcartesian" ) && ( params.interpolation_order == 2 ) ) {
 //             if( !vectorization ) {
-//                Interp = new Interpolator1D2Order( params, patch );
+//                if( !params.interpolation_WT ) {
+//                    Interp = new Interpolator1D2Order( params, patch );
+//                }
+//                else {
+//                    Interp = new Interpolator1DWT2Order( params, patch );
+//                    wt_used = true;
+//                }
 //             }
 // #ifdef _VECTO
 //             else {
-                 Interp = new Interpolator1D2OrderV( params, patch );
+                if( !params.interpolation_WT ) {
+                    Interp = new Interpolator1D2OrderV( params, patch );
+                }
+                else {
+                    Interp = new Interpolator1DWT2OrderV( params, patch );
+                    wt_used = true;
+                }
             // }
 // #endif
         } else if( ( params.geometry == "1Dcartesian" ) && ( params.interpolation_order == 4 ) ) {
-            Interp = new Interpolator1D4Order( params, patch );
+            if( !params.interpolation_WT ) {
+                Interp = new Interpolator1D4Order( params, patch );
+            }
+            else {
+                Interp = new Interpolator1DWT4Order( params, patch );
+                wt_used = true;
+            }
         }
         // ---------------
         // 2Dcartesian simulation
         // ---------------
         else if( ( params.geometry == "2Dcartesian" ) && ( params.interpolation_order == 2 ) ) {
             if( !vectorization ) {
-                Interp = new Interpolator2D2Order( params, patch );
+                if( !params.interpolation_WT ) {
+                    Interp = new Interpolator2D2Order( params, patch );
+                }
+                else {
+                    Interp = new Interpolator2DWT2Order( params, patch );
+                    wt_used = true;
+                }
             }
 #ifdef _VECTO
             else {
-                Interp = new Interpolator2D2OrderV( params, patch );
+                if( !params.interpolation_WT ) {
+                    Interp = new Interpolator2D2OrderV( params, patch );
+                }
+                else {
+                    Interp = new Interpolator2DWT2OrderV( params, patch );
+                    wt_used = true;
+                }
             }
 #endif
         } else if( ( params.geometry == "2Dcartesian" ) && ( params.interpolation_order == 4 ) ) {
             if( !vectorization ) {
-                Interp = new Interpolator2D4Order( params, patch );
+                if( !params.interpolation_WT ) {
+                    Interp = new Interpolator2D4Order( params, patch );
+                }
+                else {
+                    Interp = new Interpolator2DWT4Order( params, patch );
+                    wt_used = true;
+                }
             }
 #ifdef _VECTO
             else {
-                Interp = new Interpolator2D4OrderV( params, patch );
+                if( !params.interpolation_WT ) {
+                    Interp = new Interpolator2D4OrderV( params, patch );
+                }
+                else {
+                    Interp = new Interpolator2DWT4OrderV( params, patch );
+                    wt_used = true;
+                }
             }
 #endif
         }
@@ -73,20 +127,44 @@ public:
         // ---------------
         else if( ( params.geometry == "3Dcartesian" ) && ( params.interpolation_order == 2 ) ) {
             if( !vectorization ) {
-                Interp = new Interpolator3D2Order( params, patch );
+                if( !params.interpolation_WT ) {
+                    Interp = new Interpolator3D2Order( params, patch );
+                }
+                else {
+                    Interp = new Interpolator3DWT2Order( params, patch );
+                    wt_used = true;
+                }
             }
 #ifdef _VECTO
             else {
-                Interp = new Interpolator3D2OrderV( params, patch );
+                if( !params.interpolation_WT ) {
+                    Interp = new Interpolator3D2OrderV( params, patch );
+                }
+                else {
+                    Interp = new Interpolator3DWT2OrderV( params, patch );
+                    wt_used = true;
+                }
             }
 #endif
         } else if( ( params.geometry == "3Dcartesian" ) && ( params.interpolation_order == 4 ) ) {
             if( !vectorization ) {
-                Interp = new Interpolator3D4Order( params, patch );
+                if( !params.interpolation_WT ) {
+                    Interp = new Interpolator3D4Order( params, patch );
+                }
+                else {
+                    Interp = new Interpolator3DWT4Order( params, patch );
+                    wt_used = true;
+                }
             }
 #ifdef _VECTO
             else {
-                Interp = new Interpolator3D4OrderV( params, patch );
+                if( !params.interpolation_WT ) {
+                    Interp = new Interpolator3D4OrderV( params, patch );
+                }
+                else {
+                    Interp = new Interpolator3DWT4OrderV( params, patch );
+                    wt_used = true;
+                }
             }
 #endif
         }
@@ -102,6 +180,10 @@ public:
         }
         else {
             ERROR( "Unknwon parameters : " << params.geometry << ", Order : " << params.interpolation_order );
+        }
+
+        if( ( params.interpolation_WT ) && ( !wt_used ) ){
+            ERROR( "WT not implemented for Geometry: " << params.geometry << ", Order : " << params.interpolation_order );
         }
 
         return Interp;
