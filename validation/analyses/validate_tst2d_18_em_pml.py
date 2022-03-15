3b32d8f1c60366d32b7984d60a1ccafb8624a52c
Arnaud Beck <beck@llr.in2p3.fr>
2022-03-15 15:02:33 +0100

    update pml ci

--- validation/analyses/validate_tst2d_18_em_pml.py
+++ validation/analyses/validate_tst2d_18_em_pml.py
@@ -7,9 +7,9 @@ S = happi.Open(["./restart*"], verbose=False)
 
 # COMPARE THE Ez FIELD
 Ez = S.Field.Field0.Ez(timesteps=300).getData()[0]
-Validate("Ez field at iteration 300", Ez, 0.001)
+Validate("Ez field at iteration 300", Ez, 0.00002)
 
 # TEST THAT Ubal_norm STAYS OK
 uelm = S.Scalar.Uelm().getData(timestep=300)
-Validate("Uelm is below 0.2% after absorption", uelm[0]<0.002 )
+Validate("Uelm is below 0.2% after absorption", uelm[0]<0.00002 )
 
