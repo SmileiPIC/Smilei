df6cda7ad7167c6209b03b60e32b29f3b0042a66
Etienne Malaboeuf <eti.malaboeuf@gmail.com>
2022-04-07 16:57:54 +0200

    Typo & multiple omp thread error

--- src/Pusher/PusherBoris.cpp
+++ src/Pusher/PusherBoris.cpp
@@ -75,22 +75,22 @@ void PusherBoris::operator()( Particles &particles, SmileiMPI *smpi, int istart,
     const std::size_t particle_number = iend - istart;
 
     // TODO(Etienne M): Memory ops optimization
-    #pragma omp target          map(to                          \
-                                    : Ex [istart_offset:particle_number],    \
-                                      Ey [istart_offset:particle_number],    \
-                                      Ez [istart_offset:particle_number],    \
-                                      Bx [istart_offset:particle_number],    \
-                                      By [istart_offset:particle_number],    \
-                                      Bz [istart_offset:particle_number],    \
-                                      invgf [istart_offset:particle_number], \
-                                      position_x [istart:particle_number],   \
-                                      position_y [istart:particle_number],   \
-                                      position_z [istart:particle_number],   \
-                                      momentum_x [istart:particle_number],   \
-                                      momentum_y [istart:particle_number],   \
-                                      momentum_z [istart:particle_number],   \
-                                      charge [istart:particle_number])
-    #pragma omp teams /* num_teams(xxx) thread_limit(xxx) */ // TODO(Etienne M): WG/WF tuning 
+    #pragma omp target     map(tofrom                                   \
+                               : Ex [istart_offset:particle_number],    \
+                                 Ey [istart_offset:particle_number],    \
+                                 Ez [istart_offset:particle_number],    \
+                                 Bx [istart_offset:particle_number],    \
+                                 By [istart_offset:particle_number],    \
+                                 Bz [istart_offset:particle_number],    \
+                                 invgf [istart_offset:particle_number], \
+                                 position_x [istart:particle_number],   \
+                                 position_y [istart:particle_number],   \
+                                 position_z [istart:particle_number],   \
+                                 momentum_x [istart:particle_number],   \
+                                 momentum_y [istart:particle_number],   \
+                                 momentum_z [istart:particle_number],   \
+                                 charge [istart:particle_number])
+    #pragma omp            teams /* num_teams(xxx) thread_limit(xxx) */ // TODO(Etienne M): WG/WF tuning
     #pragma omp distribute parallel for
 #elif defined(_GPU)
     const std::size_t istart_offset   = istart - ipart_buffer_offset;
--- src/Smilei.cpp
+++ src/Smilei.cpp
@@ -117,17 +117,22 @@ int main( int argc, char *argv[] )
         ERROR("Smilei was compiled to offload computation to a GPU. Please enable the GPU mode in the Python input file.");
     } else {
 
-        const int the_gpu_count = ::omp_get_num_devices();
+        if(::omp_get_max_threads() != 1) {
+            // TODO(Etienne M): We should be able to work around this "problem"
+            ERROR("Runing Smilei on GPU using more than one OpenMP thread is not supported when offloading using OpenMP.");
+        }
 
-        if(the_gpu_count < 1) {
+        const int gpu_count = ::omp_get_num_devices();
+
+        if(gpu_count < 1) {
             ERROR("Simlei needs one accelerator, none detected.");
-        } else if(the_gpu_count > 1) {
+        } else if(gpu_count > 1) {
             WARNINGALL("Simlei needs only one accelerator (GPU). You could use --gpu-bind=per_task:1 or --gpus-per-task=1 in your slurm script.");
             WARNINGALL("Smilei will fallback to round robin GPU binding using it's MPI rank.");
 
-            const int this_process_gpu = smpi.getRank() % the_gpu_count;
+            const int this_process_gpu = smpi.getRank() % gpu_count;
 
-            std::cout << "Using GPU id: " << this_process_gpu << "\n";
+            // std::cout << "Using GPU id: " << this_process_gpu << "\n";
 
             ::omp_set_default_device(this_process_gpu);
         } else {
