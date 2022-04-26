b4d1dca583669a53d731e5e986731597128b609f
Mathieu Lobet <mathieu.lobet@cea.fr>
2022-04-26 16:47:26 +0200

    Small fix

--- src/Tools/gpu_rand.h
+++ src/Tools/gpu_rand.h
@@ -2,9 +2,9 @@
 #define GPU_RAND
 
 #if defined(_GPU)
-    #include <hiprand.hpp>
-#elif defined(SMILEI_ACCELERATOR_GPU_OMP)
     #include <openacc_curand.h>
+#elif defined(SMILEI_ACCELERATOR_GPU_OMP)
+    #include <hiprand.hpp>
 #endif
 
 #endif
