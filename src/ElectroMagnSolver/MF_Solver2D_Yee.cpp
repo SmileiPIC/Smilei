6b711aa4c9533ffede8f1bc192b3737b727dd731
Etienne Malaboeuf <eti.malaboeuf@gmail.com>
2022-07-08 12:44:31 +0000

    Validated MF_Solver2D_Yee on GPU

--- src/ElectroMagnSolver/MF_Solver2D_Yee.cpp
+++ src/ElectroMagnSolver/MF_Solver2D_Yee.cpp
@@ -30,6 +30,14 @@ void MF_Solver2D_Yee::operator()( ElectroMagn *fields )
     double *const __restrict__ Bz2D       = fields->Bz_->data();                    // [x * ny_d + y] : dual in x,y primal in z
 
     // Magnetic field Bx^(p,d)
+#if defined( SMILEI_ACCELERATOR_GPU_OMP_PENDING )
+    #pragma omp target map( tofrom                                          \
+                            : Bx2D [1:( nx_d - 1 - 1 ) * ny_d + ny_d - 1] ) \
+        map( to                                                             \
+             : Ez2D [1:(nx_d - 1 - 1) * ny_p + ny_d - 1] )
+    #pragma omp teams
+    #pragma omp distribute parallel for collapse( 2 )
+#endif
     for( unsigned int x = 0; x < nx_d - 1; ++x ) {
         for( unsigned int y = 1; y < ny_d - 1; ++y ) {
             Bx2D[x * ny_d + y] -= dt_ov_dy * ( Ez2D[x * ny_p + y] - Ez2D[x * ny_p + y - 1] );
@@ -37,6 +45,14 @@ void MF_Solver2D_Yee::operator()( ElectroMagn *fields )
     }
 
     // Magnetic field By^(d,p)
+#if defined( SMILEI_ACCELERATOR_GPU_OMP_PENDING )
+    #pragma omp target map( tofrom                                         \
+                            : By2D [ny_p:( nx_d - 1 - 1 ) * ny_p + ny_p] ) \
+        map( to                                                            \
+             : Ez2D [ny_p:( nx_d - 1 - 1 ) * ny_p + ny_p] )
+    #pragma omp teams
+    #pragma omp distribute parallel for collapse( 2 )
+#endif
     for( unsigned int x = 1; x < nx_d - 1; ++x ) {
         for( unsigned int y = 0; y < ny_p; ++y ) {
             By2D[x * ny_p + y] += dt_ov_dx * ( Ez2D[x * ny_p + y] - Ez2D[( x - 1 ) * ny_p + y] );
@@ -44,6 +60,15 @@ void MF_Solver2D_Yee::operator()( ElectroMagn *fields )
     }
 
     // Magnetic field Bz^(d,d)
+#if defined( SMILEI_ACCELERATOR_GPU_OMP_PENDING )
+    #pragma omp target map( tofrom                                                 \
+                            : Bz2D [ny_d + 1:( nx_d - 1 - 1 ) * ny_d + ny_d - 1] ) \
+        map( to                                                                    \
+             : Ex2D [ny_p + 1:( nx_d - 1 - 1 ) * ny_p + ny_d - 1],                 \
+               Ey2D [ny_d + 1:( nx_d - 1 - 1 ) * ny_d + ny_d - 1] )
+    #pragma omp teams
+    #pragma omp distribute parallel for collapse( 2 )
+#endif
     for( unsigned int x = 1; x < nx_d - 1; ++x ) {
         for( unsigned int y = 1; y < ny_d - 1; ++y ) {
             Bz2D[x * ny_d + y] += dt_ov_dy * ( Ex2D[x * ny_p + y] - Ex2D[x * ny_p + y - 1] ) -
