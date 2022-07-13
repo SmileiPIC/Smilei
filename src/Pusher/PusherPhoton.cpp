ad8c7f210fe90e86c375a7dbea996e2b23d750dc
Mathieu Lobet <mathieu.lobet@cea.fr>
2022-07-13 14:19:46 +0200

    correct compilation issue for JZ

--- src/Pusher/PusherPhoton.cpp
+++ src/Pusher/PusherPhoton.cpp
@@ -44,7 +44,8 @@ void PusherPhoton::operator()( Particles &particles, SmileiMPI *smpi,
     #ifndef _GPU
         #pragma omp simd
     #else
-        int np = iend-istart;
+        //int np = iend-istart;
+        int nparts = smpi->dynamics_invgf[ithread].size();
         #pragma acc parallel present(invgf[0:nparts]) deviceptr(position_x,position_y,position_z,momentum_x,momentum_y,momentum_z)
         #pragma acc loop gang worker vector
     #endif
