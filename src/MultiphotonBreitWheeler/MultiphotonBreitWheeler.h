9dbecfd5957cf9b94c5153fd0ccbe5f681a22f98
Mathieu Lobet <mathieu.lobet@cea.fr>
2022-08-05 17:01:12 +0200

    Few updates for mBW preparation for gpu

--- src/MultiphotonBreitWheeler/MultiphotonBreitWheeler.h
+++ src/MultiphotonBreitWheeler/MultiphotonBreitWheeler.h
@@ -124,7 +124,7 @@ public:
         int *bmin, int *bmax, int ithread );
 
     //! Return the sampling for each pair 
-    int get_pair_creation_sampling(int i) {
+    int getPairCreationSampling(int i) {
         return mBW_pair_creation_sampling_[i];
     }
 
