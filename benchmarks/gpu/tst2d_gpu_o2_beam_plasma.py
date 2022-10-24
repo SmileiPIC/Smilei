1022bfd3d1051ef1e06b1da3b9377161c3cf3f0f
Etienne Malaboeuf <eti.malaboeuf@gmail.com>
2022-10-24 13:48:18 +0200

    Better OpenMP version restriction

--- benchmarks/gpu/tst2d_gpu_o2_beam_plasma.py
+++ benchmarks/gpu/tst2d_gpu_o2_beam_plasma.py
@@ -39,12 +39,12 @@ dt  = 0.5 * dx/m.sqrt(2.)
 number_part_per_cell = 180 # 1800
 rapport_de_masse=1836.
 
-NumberOfCell_inX=1024
-NumberOfCell_inY=1024
+NumberOfCell_inX=256
+NumberOfCell_inY=256
 OutputEveryNumberOfCellForFieldData=1 # on sauvegarde les point grille des fichiers grilles
 
 # 16 | 128
-SizePatch=256
+SizePatch=128
 
 #################################
 NumberOfTimeStep=600
