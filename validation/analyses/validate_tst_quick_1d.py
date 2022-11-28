74621646a7022659c44a3d315387a572e9b8e94b
xxirii <>
2022-11-28 16:30:36 +0100

    GitHub action: add a quick 1d test to run the code

---
+++ validation/analyses/validate_tst_quick_1d.py
@@ -0,0 +1,54 @@
+import os, re, numpy as np, math
+import happi
+
+S = happi.Open(["./restart*"], verbose=False)
+
+# Get some Smilei parameters
+timestep = S.namelist.Main.timestep
+simulation_time = S.namelist.Main.simulation_time
+
+diag_every = int(simulation_time / timestep)
+
+species_list = ["eon1", "pon1", "eon2", "pon2"]
+
+Scalar = {}
+
+relative_error = 0.05
+
+for species in species_list:
+    name = "Ntot_{}".format(species)
+    Scalar[name] = np.array(S.Scalar(name).getData())
+    for index,value in enumerate(Scalar[name]):
+        Validate("Scalar {}[{}]".format(name,index) , value, value*relative_error)
+    
+    name = "Ukin_{}".format(species)
+    Scalar[name] = np.array(S.Scalar(name).getData())
+    for index,value in enumerate(Scalar[name]):
+        Validate("Scalar {}[{}]".format(name,index) , value, value*relative_error)
+
+    name = "Dens_{}".format(species)
+    Scalar[name] = np.array(S.Scalar(name).getData())
+    for index,value in enumerate(Scalar[name]):
+        Validate("Scalar {}[{}]".format(name,index) , value, value*relative_error)
+
+# Energy _________________________________________________________________
+
+for i in range(4):
+    particle_binning_initial = S.ParticleBinning(diagNumber=i,timesteps=0)
+    data_initial = np.array(particle_binning_initial.getData()[0])
+    
+    particle_binning = S.ParticleBinning(diagNumber=i,timesteps=diag_every)
+    data_final = np.array(particle_binning.getData()[0])
+    
+    sum_energy = np.sum(data_final)
+
+    Validate("Sum of the gamma spectrum for {}".format(species_list[i]) , sum_energy, sum_energy*relative_error)
+    
+    sum_initial = np.sum(data_initial)
+    sum_final = np.sum(data_final)
+    
+    error = (np.abs(np.subtract(data_initial / sum_initial, data_final / sum_final)))
+    
+    print(' Gamma spectrum max error for {}: {}'.format(species_list[i], np.max(error)))
+    
+    Validate("Gamma spectrum max error for {}".format(species_list[i]) , np.max(error), relative_error)
