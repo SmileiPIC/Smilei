6aef51efadcd80c4e642b9bcd19c8b12fe7f8ff0
massimo <massimo@llrlsi-gw.in2p3.fr>
2023-05-18 09:03:28 +0200

    add B-TIS3 to benchmark without envelope

--- validation/analyses/validate_tstAM_04b_laser_propagation_numpy.py
+++ validation/analyses/validate_tstAM_04b_laser_propagation_numpy.py
@@ -10,3 +10,8 @@ Validate("Plasma density at iteration 0", N0, 0.001)
 # COMPARE THE PLASMA DENSITY @ t=1500
 N1 = S.ParticleBinning(0,sum={"z":"all"}, timesteps=1500).getData()[0]
 Validate("Plasma density at iteration 1500", N1, 0.001)
+
+
+# COMPARE THE Ex FIELD
+BzBTIS3 = S.Probe.Probe0.BzBTIS3(timesteps=2000.).getData()[0][::][::8,::8]
+Validate("BzBTIS3 Probe at iteration 350", BzBTIS3, 0.01)
