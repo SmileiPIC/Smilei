6766bbf3484a3f9e8b327cf9d8bb96a8fac4b2ad
Francesco Massimo <francescomassimo@MacBook-Pro-de-sei2.local>
2024-05-08 15:41:06 +0200

    Revert "apply optimized longitudinal derivative also to Maxwell Ampere solver"
    This reverts commit 473796aba25b547c4f313bef8e657f835e570f65.

--- src/ElectroMagnSolver/SolverFactory.h
+++ src/ElectroMagnSolver/SolverFactory.h
@@ -9,7 +9,6 @@
 #include "MA_Solver3D_Friedman.h"
 #include "MA_SolverAM_norm.h"
 #include "MA_SolverAM_Friedman.h"
-#include "MA_SolverAM_Terzani.h"
 #include "MF_Solver1D_Yee.h"
 #include "MF_Solver2D_Yee.h"
 #include "MF_Solver3D_Yee.h"
@@ -110,11 +109,7 @@ public:
                     if (params.maxwell_sol != "Yee") ERROR( "Only Yee Maxwell solver is compatible with Friedman filter in AMcylindrical geometry" );
                     solver = new MA_SolverAM_Friedman( params );
                 } else {
-                    if (params.maxwell_sol == "Yee"){
-                        solver = new MA_SolverAM_norm( params );
-                    } else if (params.maxwell_sol == "Terzani"){
-                        solver = new MA_SolverAM_Terzani( params );
-                    }
+                    solver = new MA_SolverAM_norm( params );
                 }
 
             }
